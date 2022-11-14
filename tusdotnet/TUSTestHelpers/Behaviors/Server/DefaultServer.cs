87da1cee81f1bf2609adbf414b0d48a5610ad12e
smashpro666 <114948501+smashpro666@users.noreply.github.com>
2022-11-13 20:42:04 -0500

    tusdotnet client and server sample

---
+++ tusdotnet/TUSTestHelpers/Behaviors/Server/DefaultServer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using tusdotnet;
+using tusdotnet.Interfaces;
+using tusdotnet.Models;
+using tusdotnet.Models.Configuration;
+using tusdotnet.Stores;
+
+namespace TUSTestHelpers.Behaviors.Server
+{
+    public class DefaultServer
+    {
+
+        public static Task ProcessPostUpload(ITusFile file, FileCompleteContext eventContext) {
+            //react to a completed upload here...
+            //await DoSomeProcessing(content, metadata);
+            return Task.CompletedTask;
+        }
+
+    }
+}
