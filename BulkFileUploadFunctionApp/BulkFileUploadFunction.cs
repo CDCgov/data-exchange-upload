11bd86d5d25db0bd45e5a19f7afa2d4e7f1c35bb
Matt Krystof <mattkrystof@gmail.com>
2023-01-26 10:17:26 -0600

    Changes in progress to add copy to edav step

--- BulkFileUploadFunctionApp/BulkFileUploadFunction.cs
+++ BulkFileUploadFunctionApp/BulkFileUploadFunction.cs
@@ -7,6 +7,11 @@ using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
 using BulkFileUploadFunctionApp.Model;
+using Google.Protobuf.WellKnownTypes;
+using System.Security.Principal;
+using Azure.Identity;
+using Azure.Storage;
+using Azure.Storage.Sas;
 
 namespace BulkFileUploadFunctionApp
 {
@@ -42,7 +47,7 @@ namespace BulkFileUploadFunctionApp
 
                 GetRequiredMetaData(tusInfoFile, out string filename, out string destinationId, out string extEvent);
 
-                // Partioning is part of the filename where slashes will create subfolders.
+                // Partitioning is part of the filename where slashes will create subfolders.
                 // Container name is "{meta_destination_id}-{extEvent}"
                 // Path inside of that is year / month / day / filename
                 var dateTimeNow = DateTime.UtcNow;
@@ -53,13 +58,46 @@ namespace BulkFileUploadFunctionApp
 
                 // There are some restrictions on container names -- underscores not allowed, must be all lowercase
                 var destinationContainerName = $"{destinationId.ToLower()}-{extEvent.ToLower()}";
-                
+
                 var tusFileMetadata = tusInfoFile?.MetaData ?? new Dictionary<string, string>();
                 tusFileMetadata.Add("tus_tguid", tusPayloadFilename);
                 tusFileMetadata.Remove("filename");
                 tusFileMetadata.Add("orig_filename", filename);
 
+                // Copy the partitioned file to DeX storage account
                 await CopyBlobAsync(connectionString, sourceContainerName, tusPayloadPathname, destinationContainerName, destinationBlobFilename, tusFileMetadata);
+
+                // Now copy the partitioned file to the EDAV storage account
+                const string edavStorageAccount = "edavdevdatalakedex";
+                // Using Service Principal
+                const string appID = "bd468706-837d-49ff-8aa0-90ce81601291"; // OCIO-EDEEDAV-DEV
+                const string appSec = "";
+                const string tenantID = "9ce70869-60db-44fd-abe8-d2767077fc8f"; // same tenant for all of CDC including DeX and EDAV
+
+                var clientCred = new ClientSecretCredential(tenantID, appID, appSec);
+
+                var edavBlobServiceClient = new BlobServiceClient(
+                    new Uri($"https://{edavStorageAccount}.blob.core.windows.net"),
+                    clientCred
+                );
+
+                var edavContainerClient = edavBlobServiceClient.GetBlobContainerClient($"{destinationContainerName}-dev");
+
+                StorageSharedKeyCredential storageSharedKeyCredential = new("dataexchangedev", "lVvJbZ5J+SvLvWpUMwybFKnqYs57J4EF+HBvWTUo9GAHsLheFRWHOxXmVmy2Ojy7m/W8qBbgXIoe+AStzh0IdQ==");
+                Uri blobContainerUri = new($"https://dataexchangedev.blob.core.windows.net/{destinationContainerName}");
+                BlobContainerClient dexCombinedSourceContainerClient = new(blobContainerUri, storageSharedKeyCredential);
+
+                BlobClient dexSourceBlob = dexCombinedSourceContainerClient.GetBlobClient(destinationBlobFilename);
+                var dexSasUri = GetServiceSasUriForBlob(dexSourceBlob);
+
+                BlobClient edavDestBlobClient = edavContainerClient.GetBlobClient(destinationBlobFilename);
+
+                _logger.LogInformation("Starting blob copy to edav...");
+
+                // Start the copy operation.
+                await edavDestBlobClient.StartCopyFromUriAsync(dexSasUri, tusFileMetadata);
+
+                _logger.LogInformation("Finished blob copy to edav");
             }
             catch (Exception e)
             {
@@ -86,7 +124,7 @@ namespace BulkFileUploadFunctionApp
             {
                 throw new TusInfoFileException("File is missing");
             }
-            
+
             _logger.LogInformation("File exists, getting lease on file");
 
             BlobLeaseClient lease = sourceBlob.GetBlobLeaseClient();
@@ -104,10 +142,114 @@ namespace BulkFileUploadFunctionApp
                 // Release the lease on the source blob
                 await lease.ReleaseAsync();
             }
-                
+
             _logger.LogInformation($"Info file metadata keys: {tusInfoFile.MetaData?.Keys}");
-            
+
             return tusInfoFile;
+        }
+
+        private Uri? GetServiceSasUriForBlob(BlobClient blobClient, string? storedPolicyName = null)
+        {
+            // Check whether this BlobClient object has been authorized with Shared Key.
+            if (blobClient.CanGenerateSasUri)
+            {
+                // Create a SAS token that's valid for one hour.
+                BlobSasBuilder sasBuilder = new()
+                {
+                    BlobContainerName = blobClient.GetParentBlobContainerClient().Name,
+                    BlobName = blobClient.Name,
+                    Resource = "b"
+                };
+
+                if (storedPolicyName == null)
+                {
+                    sasBuilder.ExpiresOn = DateTimeOffset.UtcNow.AddHours(1);
+                    sasBuilder.SetPermissions(BlobSasPermissions.Read |
+                        BlobSasPermissions.Write);
+                }
+                else
+                {
+                    sasBuilder.Identifier = storedPolicyName;
+                }
+
+                Uri sasUri = blobClient.GenerateSasUri(sasBuilder);
+                _logger.LogInformation($"SAS URI for blob is: {sasUri}");
+
+                return sasUri;
+            }
+            else
+            {
+                _logger.LogError("BlobClient must be authorized with Shared Key credentials to create a service SAS.");
+                return null;
+            }
+        }
+
+        private async Task CopyBlobAsync(BlobContainerClient sourceContainerClient, string sourceBlobName, string destinationBlobName, BlobContainerClient destinationContainerClient, IDictionary<string, string> destinationMetadata)
+        {
+            try
+            {
+                _logger.LogInformation($"Starting dex > edav file copy with source uri {sourceContainerClient.Uri}, destination uri {destinationContainerClient.Uri}");
+
+                // Create a BlobClient representing the source blob to copy.
+                BlobClient sourceBlob = sourceContainerClient.GetBlobClient(sourceBlobName);
+
+                _logger.LogInformation($"Checking if source blob with uri {sourceBlob.Uri} exists");
+
+                // Ensure that the source blob exists.
+                if (await sourceBlob.ExistsAsync())
+                {
+                    _logger.LogInformation("File exists, getting lease on file");
+
+                    // Lease the source blob for the copy operation 
+                    // to prevent another client from modifying it.
+                    BlobLeaseClient lease = sourceBlob.GetBlobLeaseClient();
+
+                    // Specifying -1 for the lease interval creates an infinite lease.
+                    await lease.AcquireAsync(TimeSpan.FromSeconds(-1));
+
+                    // Get the source blob's properties and display the lease state.
+                    BlobProperties sourceProperties = await sourceBlob.GetPropertiesAsync();
+                    _logger.LogInformation($"Lease state: {sourceProperties.LeaseState}");
+
+                    _logger.LogInformation($"edav destination filename = {destinationBlobName}");
+
+                    // Get a BlobClient representing the destination blob with a unique name.
+                    BlobClient destBlob =
+                        destinationContainerClient.GetBlobClient(destinationBlobName);
+
+                    _logger.LogInformation($"Starting blob copy, destination blob name = {destinationBlobName}");
+
+                    // Start the copy operation.
+                    await destBlob.StartCopyFromUriAsync(sourceBlob.Uri, destinationMetadata);
+
+                    _logger.LogInformation("Finished blob copy");
+
+                    // Get the destination blob's properties and display the copy status.
+                    BlobProperties destProperties = await destBlob.GetPropertiesAsync();
+
+                    _logger.LogInformation($"Copy status: {destProperties.CopyStatus}");
+                    _logger.LogInformation($"Copy progress: {destProperties.CopyProgress}");
+                    _logger.LogInformation($"Completion time: {destProperties.CopyCompletedOn}");
+                    _logger.LogInformation($"Total bytes: {destProperties.ContentLength}");
+
+                    // Update the source blob's properties.
+                    sourceProperties = await sourceBlob.GetPropertiesAsync();
+
+                    if (sourceProperties.LeaseState == LeaseState.Leased)
+                    {
+                        // Release the lease on the source blob
+                        await lease.ReleaseAsync();
+
+                        // Update the source blob's properties to check the lease state.
+                        sourceProperties = await sourceBlob.GetPropertiesAsync();
+                        _logger.LogInformation($"Lease state: {sourceProperties.LeaseState}");
+                    }
+                }
+            }
+            catch (RequestFailedException ex)
+            {
+                _logger.LogError(ex.Message);
+            }
         }
 
         private async Task CopyBlobAsync(string connectionString, string sourceContainerName, string sourceBlobName, string destinationContainerName,
