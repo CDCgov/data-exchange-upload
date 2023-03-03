b28931396b9baab99c135f054f4ac603fc235ccd
cfarmer <cfarmer@fearless.tech>
2023-03-03 14:25:17 -0500

    fix py format

--- .github/workflows/scripts/generate-jwt.py
+++ .github/workflows/scripts/generate-jwt.py
@@ -17,10 +17,7 @@ payload = {
     'iss': args.app_id
 }
 
-print(os.environ["PRIVATE_KEY"].encode('ascii'))
-
 # Create JWT
-encoded_jwt = jwt.encode(
-    payload, os.environ["PRIVATE_KEY"].encode('ascii'), algorithm='RS256')
+encoded_jwt = jwt.encode(payload, os.environ["PRIVATE_KEY"], algorithm='RS256')
 
 print(encoded_jwt)
