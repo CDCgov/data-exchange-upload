371b51dc9a389dd4f78923256e5a272d27311964
cfarmer <cfarmer@fearless.tech>
2023-03-03 14:17:42 -0500

    use env

--- .github/workflows/scripts/generate-jwt.py
+++ .github/workflows/scripts/generate-jwt.py
@@ -1,16 +1,12 @@
 import argparse
 import jwt
 import time
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--pem', required=True)
 parser.add_argument('--app-id', required=True)
 
 args = parser.parse_args()
-
-# Open PEM
-with open(args.pem, 'rb') as pem_file:
-    signing_key = jwt.jwk_from_pem(pem_file.read())
 
 payload = {
     # Issued at time
@@ -22,6 +18,6 @@ payload = {
 }
 
 # Create JWT
-encoded_jwt = jwt.encode(payload, signing_key, algorithm='RS256')
+encoded_jwt = jwt.encode(payload, os.environ["PRIVATE_KEY"], algorithm='RS256')
 
 print(encoded_jwt)
