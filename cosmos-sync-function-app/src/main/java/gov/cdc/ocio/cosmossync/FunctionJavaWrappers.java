7c47c6c28882720f64b9005ed4bfc930a2a83edb
Linda Zhou <utf8@cdc.gov>
2023-07-25 12:46:03 -0500

    add health check import

--- cosmos-sync-function-app/src/main/java/gov/cdc/ocio/cosmossync/FunctionJavaWrappers.java
+++ cosmos-sync-function-app/src/main/java/gov/cdc/ocio/cosmossync/FunctionJavaWrappers.java
@@ -3,6 +3,7 @@ package gov.cdc.ocio.cosmossync;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 import gov.cdc.ocio.cosmossync.functions.CosmosSyncFunction;
+import gov.cdc.ocio.cosmossync.functions.HealthCheckFunction;
 
 public class FunctionJavaWrappers {
 
