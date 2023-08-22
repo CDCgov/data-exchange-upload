f4c8e8f19e66e8b02f3cbd85196ba4f98064e03e
Rohit Panwar <rohitpanwar@gmail.com>
2023-08-22 15:11:17 -0400

    ndlp filename metadata issue

--- tus/file-hooks/upload-status/post_receive_bin.py
+++ tus/file-hooks/upload-status/post_receive_bin.py
@@ -89,6 +89,7 @@ def post_receive(tguid, offset, size, metadata_json):
     try:
         logger.info('python version = {0}'.format(sys.version))
         logger.info('metadata_json = {0}'.format(metadata_json))
+
         metadata = json.loads(metadata_json, object_hook=lambda d: SimpleNamespace(**d))
 
         filename = None
