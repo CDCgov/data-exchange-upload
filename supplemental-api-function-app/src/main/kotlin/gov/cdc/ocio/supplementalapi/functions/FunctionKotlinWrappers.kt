bbc46b12f432e95d46789bdc5567baca993ea218
Hiwot Gebre <130766890+USZ7@users.noreply.github.com>
2023-09-06 10:15:46 -0400

    upload-408 remove java wrappers for supp api (#141)
    * upload-408 remove java wrappers for supp api
    
    * upload-408 add package name
    
    ---------
    
    Co-authored-by: Gebre <USZ7-SU@cdc.gov>

---
+++ supplemental-api-function-app/src/main/kotlin/gov/cdc/ocio/supplementalapi/functions/FunctionKotlinWrappers.kt
@@ -0,0 +1,54 @@
+package gov.cdc.ocio.supplementalapi.functions
+import com.microsoft.azure.functions.*
+import com.microsoft.azure.functions.HttpMethod
+import com.microsoft.azure.functions.annotation.AuthorizationLevel
+import com.microsoft.azure.functions.annotation.BindingName
+import com.microsoft.azure.functions.annotation.FunctionName
+import com.microsoft.azure.functions.annotation.HttpTrigger
+import gov.cdc.ocio.supplementalapi.functions.HealthCheckFunction
+import gov.cdc.ocio.supplementalapi.functions.StatusForDestinationFunction
+import gov.cdc.ocio.supplementalapi.functions.StatusForTguidFunction
+import java.util.Optional
+
+class FunctionKotlinWrappers {
+    @FunctionName("HealthCheck")
+    fun healthCheck(
+        @HttpTrigger(
+            name = "req",
+            methods = [HttpMethod.GET],
+            route = "status/health",
+            authLevel = AuthorizationLevel.ANONYMOUS
+        ) request: HttpRequestMessage<Optional<String>>,
+        context: ExecutionContext
+    ): HttpResponseMessage {
+        return HealthCheckFunction().run(request, context)
+    }
+
+    @FunctionName("StatusForTguid")
+    fun statusForTguid(
+        @HttpTrigger(
+            name = "req",
+            methods = [HttpMethod.GET],
+            route = "status/{tguid}",
+            authLevel = AuthorizationLevel.FUNCTION
+        ) request: HttpRequestMessage<Optional<String>>,
+        @BindingName("tguid") tguid: String,
+        context: ExecutionContext
+    ): HttpResponseMessage {
+        return StatusForTguidFunction().run(request, tguid, context)
+    }
+
+    @FunctionName("StatusForDestination")
+    fun statusForDestination(
+        @HttpTrigger(
+            name = "req",
+            methods = [HttpMethod.GET],
+            route = "status/destination/{destinationName}",
+            authLevel = AuthorizationLevel.FUNCTION
+        ) request: HttpRequestMessage<Optional<String>>,
+        @BindingName("destinationName") destinationName: String,
+        context: ExecutionContext
+    ): HttpResponseMessage {
+        return StatusForDestinationFunction().run(request, destinationName, context)
+    }
+}
