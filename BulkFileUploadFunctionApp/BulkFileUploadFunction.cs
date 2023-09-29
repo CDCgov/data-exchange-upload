a017c3f5ff74683dd470b3356cdf21ec5b938c19
Rohit Panwar <rohitpanwar@gmail.com>
2023-09-29 13:08:17 -0400

    printing exception stack strace

--- BulkFileUploadFunctionApp/BulkFileUploadFunction.cs
+++ BulkFileUploadFunctionApp/BulkFileUploadFunction.cs
@@ -346,8 +346,10 @@ namespace BulkFileUploadFunctionApp
             {                
                 string currentStackTrace = System.Environment.StackTrace;
 
+                _logger.LogError("Failed to copy " +  ex.GetType().Name);
+                _logger.LogError("Failed to copy",  ex.GetType().Name);
                 _logger.LogError("Failed to copy", currentStackTrace);
-                _logger.LogError("Failed to copy", ex.ToString());
+                -logger.LogError("Failed to copy", ex.ToString());
                 _logger.LogError("Failed to copy", ex.StackTrace);
                 
                 if ((ex is AggregateException aggEx) && (aggEx.InnerExceptions != null))
