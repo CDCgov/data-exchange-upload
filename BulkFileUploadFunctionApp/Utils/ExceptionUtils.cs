938ec5d6e41008883c6dfb7d78fd9924fb809553
Rohit Panwar <rohitpanwar@gmail.com>
2023-10-01 01:11:08 -0400

    printing exception stack strace

--- BulkFileUploadFunctionApp/Utils/ExceptionUtils.cs
+++ BulkFileUploadFunctionApp/Utils/ExceptionUtils.cs
@@ -7,7 +7,7 @@ namespace BulkFileUploadFunctionApp.Utils
         public static void LogErrorDetails(Exception ex, ILogger _logger)
         {
             if(ex is AggregateException) {
-                foreach (var inner in ex.Flatten().InnerExceptions) {
+                foreach (var inner in (AggregateException) ex.Flatten().InnerExceptions) {
                     _logger.LogError(inner.ToString());
                 }
             } else {
