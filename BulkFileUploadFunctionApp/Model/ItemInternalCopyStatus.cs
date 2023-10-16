8e70c1722245bd5aa9d0814879baca51bc4bc997
lrp4 <lrp4@cdc.gov>
2023-10-16 10:35:26 -0400

    copy status fn change to item internal copy status, added model in C#

---
+++ BulkFileUploadFunctionApp/Model/ItemInternalCopyStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace BulkFileUploadFunctionApp.Model
+
+{
+    public class ItemInternalCopyStatus
+    {
+        public string? Tguid { get; set; }
+
+        public string? StatusDEX { get; set; }
+
+        public string? StatusEDAV { get; set; }
+
+    }
+}
