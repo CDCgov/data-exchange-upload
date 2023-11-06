448cfdb9c138e9b1211ff3997cc40990212c32f4
Rohit Panwar <rohitpanwar@rohits-mac-mini.lan>
2023-11-06 16:35:18 -0500

    json logger

--- supplemental-api-function-app/src/main/kotlin/gov/cdc/ocio/supplementalapi/functions/HealthCheckFunction.kt
+++ supplemental-api-function-app/src/main/kotlin/gov/cdc/ocio/supplementalapi/functions/HealthCheckFunction.kt
@@ -9,6 +9,7 @@ import gov.cdc.ocio.supplementalapi.cosmos.CosmosClientManager
 import gov.cdc.ocio.supplementalapi.model.Item
 import java.util.*
 import com.azure.cosmos.CosmosClient
+import mu.KotlinLogging
 
 
 
@@ -21,7 +22,10 @@ class HealthCheckFunction {
         cosmosClient: CosmosClient
     ): HttpStatus {
 
+        val logger = KotlinLogging.logger {}           
+        
         try {
+                
             val databaseName = System.getenv("CosmosDbDatabaseName")
             val containerName = System.getenv("CosmosDbContainerName")
 
@@ -34,10 +38,16 @@ class HealthCheckFunction {
                 Item::class.java
             )
 
+           logger.info("instance healthy")
+
            return HttpStatus.OK
 
         } catch (ex: Exception) {
-            println("An error occurred: ${ex.message}")
+
+            logger.info("instance not healthy")
+            logger.error(ex.message)
+
+            //println("An error occurred: ${ex.message}")
 
             return HttpStatus.INTERNAL_SERVER_ERROR
         }
