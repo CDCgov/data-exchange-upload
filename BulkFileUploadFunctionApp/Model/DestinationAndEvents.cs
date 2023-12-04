d6273dcd81104b0bd53df955a0f3407ec9dc53ab
rohitpanwar <rohitpanwar@users.noreply.github.com>
2023-12-04 08:26:52 -0500

    668/copy to routing (#186)
    * copy to routing

---
+++ BulkFileUploadFunctionApp/Model/DestinationAndEvents.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Serialization;
+
+namespace BulkFileUploadFunctionApp.Model
+{
+    public class DestinationAndEvents
+    {
+        [JsonPropertyName("destination_id")]
+        public string? destinationId { get; set; }
+        
+        [JsonPropertyName("ext_events")]
+        public List<ExtEvent>? extEvents { get; set; }
+
+        public static readonly DestinationAndEvents Default = new DestinationAndEvents();
+    }
+
+    public class ExtEvent
+    {
+        public string? name { get; set; }
+
+        [JsonPropertyName("definition_filename")]
+        public string? definitionFilename { get; set; }
+
+        [JsonPropertyName("copy_targets")]
+        public List<CopyTarget>? copyTargets { get; set; }
+    }
+    
+    public class CopyTarget
+    {
+        public string? target { get; set; }
+    } 
+}
