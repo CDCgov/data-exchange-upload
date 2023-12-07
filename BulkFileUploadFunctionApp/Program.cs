6d31dd53a82ce85d232336036482d5e0a11e471f
Gebre <USZ7-SU@cdc.gov>
2023-12-07 10:26:16 -0500

    upload-563 use microsoft console log formatter

--- BulkFileUploadFunctionApp/Program.cs
+++ BulkFileUploadFunctionApp/Program.cs
@@ -1,18 +1,10 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using Serilog.Formatting.Compact;
 
-var logger = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .WriteTo.Console(new CompactJsonFormatter())
-            .CreateLogger();
-try
-{
+
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .UseSerilog(logger)
+    .ConfigureFunctionsWorkerDefaults()    
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
@@ -21,8 +13,4 @@ var host = new HostBuilder()
 
 host.Run();
 
-}
-finally
-{
-    Log.CloseAndFlush();
-}
+
