1d7b89d6ac2f144e0c030257ebe66850980a454c
Gebre <USZ7-SU@cdc.gov>
2023-12-18 17:52:05 -0500

    upload-563 create json logger for bulk upload api

--- BulkFileUploadFunctionApp/Program.cs
+++ BulkFileUploadFunctionApp/Program.cs
@@ -1,8 +1,21 @@
+using BulkFileUploadFunctionApp;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
+    .ConfigureLogging(builder =>
+    {
+        var config = new JsonLoggerConfiguration
+        {
+            LogLevel = LogLevel.Information,
+            TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff"
+            // Configure other properties as needed
+        };
+        builder.ClearProviders();
+        builder.AddProvider(new JsonLoggerProvider(config));
+    })
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
