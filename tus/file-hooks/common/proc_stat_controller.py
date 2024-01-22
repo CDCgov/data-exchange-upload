7c6721d1214f8788352932b8b4fc67c1a9caf4c0
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2024-01-22 15:50:36 -0500

    Upload 785/upload trace start (#196)
    * boilerplate for post-create hook
    
    * gets required matadata and sends post request
    
    * requirements python file
    
    * starts the dex-upload span
    
    * proc stat controller class
    
    * pep8 fixes
    
    * more formating fixes
    
    * append common dir to sys path
    
    * basic logging
    
    * small condition cleanup

---
+++ tus/file-hooks/common/proc_stat_controller.py
@@ -0,0 +1,40 @@
+import requests
+
+
+def _handle_trace_response(resp_json):
+    if 'trace_id' not in resp_json or 'span_id' not in resp_json:
+        raise Exception('Invalid PS API response: ' + str(resp_json))
+
+
+class ProcStatController:
+    def __init__(self, url):
+        self.url = url
+
+    def create_upload_trace(self, upload_id, destination_id, event_type):
+        params = {
+            'uploadId': upload_id,
+            'destinationId': destination_id,
+            'eventType': event_type
+        }
+        response = requests.post(f'{self.url}/api/trace', params=params)
+        response.raise_for_status()
+
+        resp_json = response.json()
+
+        _handle_trace_response(resp_json)
+
+        return resp_json['trace_id'], resp_json['span_id']
+
+    def start_span_for_trace(self, trace_id, parent_span_id, stage_name):
+        params = {
+            "stageName": stage_name,
+            "spanMark": "start"
+        }
+        response = requests.put(f'{self.url}/api/trace/addSpan/{trace_id}/{parent_span_id}', params=params)
+        response.raise_for_status()
+
+        resp_json = response.json()
+
+        _handle_trace_response(resp_json)
+
+        return resp_json['trace_id'], resp_json['span_id']
