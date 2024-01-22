6166b342b97488fca4323852582a6d432925ce12
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2024-01-22 15:50:36 -0500

    Upload 785/upload trace start (#196)
    * boilerplate for post-create hook
    
    * gets required matadata and sends post request
    
    * requirements python file
    
    * starts the dex-upload span
    
    * proc stat controller class
    
    * pep8 fixes
    
    * more formating fixes
    
    * append common dir to sys path
    
    * basic logging
    
    * small condition cleanup

---
+++ tus/file-hooks/post-create/post_create_bin.py
@@ -0,0 +1,71 @@
+import argparse
+import json
+import os
+import sys
+import logging
+
+from dotenv import load_dotenv
+
+# Including the common directory because it's above the script's root directory.
+sys.path.append(os.path.join(sys.path[0], '..', 'common'))
+# IDEs will probably complain that it can't find this module.
+from proc_stat_controller import ProcStatController
+
+load_dotenv()
+
+required_metadata_fields = ['meta_destination_id', 'meta_ext_event']
+
+
+def get_required_metadata(metadata_str):
+    meta_json = json.loads(metadata_str)
+    missing_metadata_fields = []
+
+    for field in required_metadata_fields:
+        if field not in meta_json:
+            missing_metadata_fields.append(field)
+
+    if len(missing_metadata_fields) > 0:
+        raise Exception('Missing one or more required metadata fields: ' + str(missing_metadata_fields))
+
+    return [
+        meta_json['meta_destination_id'],
+        meta_json['meta_ext_event'],
+    ]
+
+
+def main():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--id')
+    parser.add_argument('-m', '--metadata')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+
+    log_level = logging.INFO
+    if args.verbose:
+      log_level = logging.DEBUG
+    
+    logging.basicConfig(level=log_level)
+    logger = logging.getLogger(__name__)
+
+    tguid = args.id
+    metadata = args.metadata
+
+    if tguid is None:
+        raise Exception('No tguid provided')
+
+    # Create upload trace.
+    dest, event = get_required_metadata(metadata)
+    logger.info(f'Creating trace for upload {tguid} with destination {dest} and event {event}')
+
+    ps_api_controller = ProcStatController(os.getenv('PS_API_URL'))
+    trace_id, parent_span_id = ps_api_controller.create_upload_trace(tguid, dest, event)
+    logger.debug(f'Created trace for upload {tguid} with trace ID {trace_id} and parent span ID {parent_span_id}')
+
+    # Start the upload child span.  Will be stopped in post-finish hook when the upload is complete.
+    ps_api_controller.start_span_for_trace(trace_id, parent_span_id, "dex-upload")
+    logger.debug(f'Created child span for parent span {parent_span_id} with stage name of dex-upload')
+
+
+if __name__ == '__main__':
+    main()
