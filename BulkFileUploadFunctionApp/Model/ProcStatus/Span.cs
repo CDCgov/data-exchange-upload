7f31d1fefb086e3c9a0834520f1d1167931582f3
cfarmer <cfarmer@fearless.tech>
2024-01-22 15:05:15 -0500

    fields for span

--- BulkFileUploadFunctionApp/Model/ProcStatus/Span.cs
+++ BulkFileUploadFunctionApp/Model/ProcStatus/Span.cs
@@ -5,5 +5,12 @@ namespace BulkFileUploadFunctionApp.Model.ProcStatus
   public class Span
   {
     public static readonly Span Default = new Span();
+
+    [JsonPropertyName("stage_name")]
+    public string? stageName { get; set; }
+    public string? timestamp { get; set; }
+    public string? status { get; set; }
+    [JsonPropertyName("ellapsed_millis")]
+    public int? ellapsedMillis { get; set; }
   }
 }
