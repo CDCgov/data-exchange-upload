c7c6cf1f1cc82586ef658c60f3af7840c24560b3
cfarmer <cfarmer@fearless.tech>
2024-01-23 08:45:17 -0500

    update service interface

--- BulkFileUploadFunctionApp/Service/IProcStatusService.cs
+++ BulkFileUploadFunctionApp/Service/IProcStatusService.cs
@@ -5,5 +5,7 @@ namespace BulkFileUploadFunctionApp.Service
   public interface IProcStatService
   {
     Task<Trace> GetTraceByUploadId(string uploadID);
+    Task StartSpanForTrace(string traceId, string parentSpanId, string stageName);
+    Task StopSpanForTrace(string traceId, string parentSpanId, string stageName);
   }
 }
