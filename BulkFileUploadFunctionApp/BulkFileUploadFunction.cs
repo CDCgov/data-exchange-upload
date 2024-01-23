1f06fa5db761acce181ab4cf3ff2ea54ef2f4e3d
Hiwot Gebre <130766890+USZ7@users.noreply.github.com>
2024-01-23 16:45:53 -0500

    upload-885 remove log for test (#200)
    * upload-885 remove log for test
    
    * uplod-885 add key
    
    * upload-885 remove routing
    
    * upload-885 add routing logic
    
    ---------
    
    Co-authored-by: Gebre <USZ7-SU@cdc.gov>

--- BulkFileUploadFunctionApp/BulkFileUploadFunction.cs
+++ BulkFileUploadFunctionApp/BulkFileUploadFunction.cs
@@ -105,11 +105,7 @@ namespace BulkFileUploadFunctionApp
         [Function("BulkFileUploadFunction")]
         public async Task Run([EventHubTrigger("%AzureEventHubName%", Connection = "AzureEventHubConnectionString", ConsumerGroup = "%AzureEventHubConsumerGroup%")] string[] eventHubTriggerEvents)
         {
-            _logger.LogInformation($"Received events count: {eventHubTriggerEvents.Count() }");
-
-            bool isRoutingEnabled = _configuration.GetValue<bool>("FeatureManagement:ROUTING");
-
-             _logger.LogInformation($"Routing Status: {isRoutingEnabled }");
+            _logger.LogInformation($"Received events count: {eventHubTriggerEvents.Count() }");            
             
             foreach (var blobCreatedEventJson in eventHubTriggerEvents) 
             {                
@@ -127,18 +123,8 @@ namespace BulkFileUploadFunctionApp
                 StorageBlobCreatedEvent blobCreatedEvent = blobCreatedEvents[0];
                 if (blobCreatedEvent == null)
                     throw new Exception("Unexpected data content of event; there should be at least one element in the array");
-
-                if (isRoutingEnabled)
-                  {
-                    await ProcessBlobCreatedEvent(blobCreatedEvent?.Data?.Url);                    
-                     
-                   }
-                else
-                  {
-                   _logger.LogInformation($"Routing is Enabled. Bypassing routing for blob: {blobCreatedEvent?.Data?.Url}");
-                  }
-
                
+                    await ProcessBlobCreatedEvent(blobCreatedEvent?.Data?.Url);
 
             } // .foreach 
 
@@ -234,6 +220,10 @@ namespace BulkFileUploadFunctionApp
 
             var currentEvent = currentDestination?.extEvents?.Find(e => e.name == extEvent);
 
+            bool isRoutingEnabled = _configuration.GetValue<bool>(".appconfig.featureflag/ROUTING");
+
+            _logger.LogInformation($"Routing Status: {isRoutingEnabled }");
+
             if(currentEvent != null && currentEvent.copyTargets != null) {
 
                 foreach (CopyTarget copyTarget in currentEvent.copyTargets)
@@ -247,8 +237,15 @@ namespace BulkFileUploadFunctionApp
 
                     } else if (copyTarget.target == _targetRouting) {
 
-                        // Now copy the file from DeX to the ROUTING storage account, also partitioned by date
-                        await CopyBlobFromDexToRoutingAsync(destinationContainerName, destinationBlobFilename, tusFileMetadata);
+                        if (isRoutingEnabled)
+                        {
+                            // Now copy the file from DeX to the ROUTING storage account, also partitioned by date
+                            await CopyBlobFromDexToRoutingAsync(destinationContainerName, destinationBlobFilename, tusFileMetadata);                     
+                         }
+                        else
+                         {
+                         _logger.LogInformation($"Routing is disabled. Bypassing routing for blob");
+                         }                        
                     }
                 }
             } else {
