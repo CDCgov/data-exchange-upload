4f1754b864112fcd181b832252bb954451084c2f
cfarmer <cfarmer@fearless.tech>
2024-01-26 16:31:35 -0500

    quick test for PS API calls

--- tus/file-hooks/post-create/test/test_post_create.py
+++ tus/file-hooks/post-create/test/test_post_create.py
@@ -1,6 +1,12 @@
 import unittest
+import sys, os
+from unittest.mock import patch
 
 from post_create_bin import get_required_metadata
+from post_create_bin import post_create
+
+sys.path.append(os.path.join(sys.path[0], '..', 'common'))
+from proc_stat_controller import ProcStatController
 
 class TestPostCreateMethods(unittest.TestCase):
   def test_should_get_required_metadata(self):
@@ -25,6 +31,16 @@ class TestPostCreateMethods(unittest.TestCase):
       get_required_metadata(metadata)
       
     self.assertIn('Missing one or more required metadata fields', str(context.exception))
+
+  @patch.object(ProcStatController, 'create_upload_trace', return_value=("test_trace", "test_span"))
+  @patch.object(ProcStatController, 'start_span_for_trace')
+  def test_should_send_create_trace_request(self, create_upload_trace_mock, start_span_for_trace_mock):
+    create_upload_trace_mock.return_value = ("test_trace", "test_span")
+
+    post_create("1234", "test_event", "5678")
+
+    self.assertEqual(create_upload_trace_mock.call_count, 1)
+    self.assertEqual(start_span_for_trace_mock.call_count, 1)
 
 if __name__ == "__main__":
   unittest.main()
