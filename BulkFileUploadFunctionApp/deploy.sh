3d2143594f45e45dadbdca7311224c57e29144b2
cfarmer <cfarmer@fearless.tech>
2024-01-26 15:15:04 -0500

    fix function app name

--- BulkFileUploadFunctionApp/deploy.sh
+++ BulkFileUploadFunctionApp/deploy.sh
@@ -1,4 +1,4 @@
 ENV=$1
 
-func azure functionapp publish ocio-ede-$ENV-bulk-file-upload-processor --csharp --force
-az functionapp config appsettings set --name ocio-ede-$ENV-bulk-file-upload-processor --resource-group ocio-ede-$ENV-moderate-hl7-rg --settings "FUNCTIONS_WORKER_RUNTIME=dotnet-isolated"
+func azure functionapp publish $ENV-upload-processor --csharp --force
+az functionapp config appsettings set --name $ENV-upload-processor --resource-group ocio-ede-$ENV-moderate-hl7-rg --settings "FUNCTIONS_WORKER_RUNTIME=dotnet-isolated"
