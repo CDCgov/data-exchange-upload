6b7a216eb6bc053986a355f430d0f70bb9b946f8
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2024-01-29 08:52:16 -0500

    Upload 944/bugfix upload processor deploy script (#204)
    * fix function app name
    
    * move test project
    
    * move both function app projects to single dir
    
    * fix working dir for ci pipeline
    
    * update parent project ref

--- BulkFileUploadFunctionApp/deploy.sh
+++
@@ -1,4 +0,0 @@
-ENV=$1
-
-func azure functionapp publish ocio-ede-$ENV-bulk-file-upload-processor --csharp --force
-az functionapp config appsettings set --name ocio-ede-$ENV-bulk-file-upload-processor --resource-group ocio-ede-$ENV-moderate-hl7-rg --settings "FUNCTIONS_WORKER_RUNTIME=dotnet-isolated"
