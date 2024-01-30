e2f697104ba8ab06beb82bf8ed6e7d7b765e35b7
cfarmer <cfarmer@fearless.tech>
2024-01-30 16:11:55 -0500

    unit test

--- tus/file-hooks/post-finish/test/test_post_finish.py
+++ tus/file-hooks/post-finish/test/test_post_finish.py
@@ -1,10 +1,19 @@
 import unittest
 from unittest.mock import patch
-
 from proc_stat_controller import ProcStatController
+from post_finish_bin import post_finish
+
 
 class TestPostFinishMethods(unittest.TestCase):
-  pass
+  @patch.object(ProcStatController, 'get_trace_by_upload_id', return_value=("test_trace", "test_span"))
+  @patch.object(ProcStatController, 'stop_span_for_trace')
+  def test_should_send_create_trace_request(self, get_trace_by_upload_id, stop_span_for_trace_mock):
+    get_trace_by_upload_id.return_value = ("test_trace", "test_span")
+
+    post_finish("1234")
+
+    self.assertEqual(get_trace_by_upload_id.call_count, 1)
+    self.assertEqual(stop_span_for_trace_mock.call_count, 1)
 
 if __name__ == "__main__":
   unittest.main()
