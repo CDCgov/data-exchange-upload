18c1c50923966b297cfca7260027b7534141849d
cfarmer <cfarmer@fearless.tech>
2024-02-01 12:57:30 -0500

    fix mocks for post finish unit tests

--- tus/file-hooks/post-finish/test/test_post_finish.py
+++ tus/file-hooks/post-finish/test/test_post_finish.py
@@ -5,14 +5,17 @@ from post_finish_bin import post_finish
 
 
 class TestPostFinishMethods(unittest.TestCase):
-  @patch.object(ProcStatController, 'get_trace_by_upload_id', return_value=("test_trace", "test_span"))
+  @patch.object(ProcStatController, 'get_span_by_upload_id')
   @patch.object(ProcStatController, 'stop_span_for_trace')
-  def test_should_send_create_trace_request(self, get_trace_by_upload_id, stop_span_for_trace_mock):
-    get_trace_by_upload_id.return_value = ("test_trace", "test_span")
+  def test_should_send_requests_to_ps_api(self, get_span_by_upload_id_mock, stop_span_for_trace_mock):
+    get_span_by_upload_id_mock.return_value = {
+      "trace_id": "1234",
+      "span_id": "5678"
+    }
 
     post_finish("1234")
 
-    self.assertEqual(get_trace_by_upload_id.call_count, 1)
+    self.assertEqual(get_span_by_upload_id_mock.call_count, 1)
     self.assertEqual(stop_span_for_trace_mock.call_count, 1)
 
 if __name__ == "__main__":
