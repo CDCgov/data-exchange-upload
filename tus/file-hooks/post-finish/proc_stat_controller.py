d57f72416df161708261bb7bdb42b4ae61de260a
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2024-02-02 10:55:31 -0500

    Quickfix/post finish race cond (#215)
    * testing time delay in post finish
    
    * use retry for ps api calls
    
    * delaying for 2 seconds
    
    * tests for retries in proc stat controller

--- tus/file-hooks/post-finish/proc_stat_controller.py
+++ tus/file-hooks/post-finish/proc_stat_controller.py
@@ -1,4 +1,10 @@
 import requests
+import os
+import logging
+import time
+from requests import Request, Session
+
+MAX_RETRIES = os.getenv("PS_API_MAX_RETRIES") or 6
 
 
 def _handle_trace_response(resp_json):
@@ -12,12 +18,19 @@ def _handle_span_response(resp_json):
 
 
 class ProcStatController:
-    def __init__(self, url):
+    def __init__(self, url, delay_s=1):
         self.url = url
+        self.delay_s = delay_s
+        self.session = Session()
+        self.retry_count = 0
+        self.logger = logging.getLogger(__name__)
+
+    def __del__(self):
+        self.session.close()
 
     def get_trace_by_upload_id(self, upload_id):
-        response = requests.get(f'{self.url}/api/trace/uploadId/{upload_id}')
-        response.raise_for_status()
+        req = Request('GET', f'{self.url}/api/trace/uploadId/{upload_id}')
+        response = self._send_request_with_retry(req.prepare())
 
         resp_json = response.json()
         _handle_trace_response(resp_json)
@@ -29,8 +42,8 @@ class ProcStatController:
             "uploadId": upload_id,
             "stageName": stage_name
         }
-        response = requests.get(f'{self.url}/api/trace/span', params=params)
-        response.raise_for_status()
+        req = Request('GET', f'{self.url}/api/trace/span', params=params)
+        response = self._send_request_with_retry(req.prepare())
 
         resp_json = response.json()
         _handle_span_response(resp_json)
@@ -38,5 +51,26 @@ class ProcStatController:
         return resp_json
 
     def stop_span_for_trace(self, trace_id, parent_span_id):
-        response = requests.put(f'{self.url}/api/trace/stopSpan/{trace_id}/{parent_span_id}')
-        response.raise_for_status()
+        req = Request('PUT', f'{self.url}/api/trace/stopSpan/{trace_id}/{parent_span_id}')
+        self._send_request_with_retry(req.prepare())
+
+    def _send_request_with_retry(self, req):
+        # Resetting the retry count.
+        self.retry_count = 0
+
+        while self.retry_count < MAX_RETRIES:
+            try:
+                resp = self.session.send(req)
+                resp.raise_for_status()
+
+                if resp.ok:
+                    # Request was handled successfully, return and don't send any more requests.
+                    return resp
+            except requests.exceptions.RequestException as e:
+                self.logger.warning(f"Error sending request to PS API after attempt {self.retry_count}.  Reason: {e}")
+                self.retry_count = self.retry_count + 1
+
+                # Waiting 2 second before trying again.
+                time.sleep(self.delay_s)
+
+        raise Exception(f"Unable to send successful request to PS API after {MAX_RETRIES} attempts.")
---
+++ tus/file-hooks/post-finish/test/test_proc_stat_controller.py
@@ -0,0 +1,28 @@
+import unittest
+from proc_stat_controller import ProcStatController
+
+
+class TestProcStatController(unittest.TestCase):
+    def test_should_retry_get_trace_when_upload_id_invalid(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with self.assertRaises(Exception) as context:
+            controller.get_trace_by_upload_id(None)
+
+        self.assertEqual(controller.retry_count, 6)
+
+    def test_should_retry_get_span_when_upload_id_invalid(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with self.assertRaises(Exception) as context:
+            controller.get_span_by_upload_id(None, 'dummy')
+
+        self.assertEqual(controller.retry_count, 6)
+
+    def test_should_retry_stop_span_when_input_invalid(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with self.assertRaises(Exception) as context:
+            controller.stop_span_for_trace(None, None)
+
+        self.assertEqual(controller.retry_count, 6)
