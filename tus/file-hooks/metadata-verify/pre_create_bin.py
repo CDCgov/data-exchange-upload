b1e1c503b8ff2471576d2569a16bcaa397b49517
cfarmer <cfarmer@fearless.tech>
2024-02-05 15:20:09 -0500

    handle verify fail for req metdata fields

--- tus/file-hooks/metadata-verify/pre_create_bin.py
+++ tus/file-hooks/metadata-verify/pre_create_bin.py
@@ -1,28 +1,44 @@
-import sys, getopt
+import getopt
 import json
+import os
+import sys
+import uuid
+import logging
 from argparse import Namespace
+
+from proc_stat_controller import ProcStatController
+
+logger = logging.getLogger(__name__)
+
+required_metadata_fields = ['meta_destination_id', 'meta_ext_event']
+
 
 class AllowedDestinationEvents:
     def __init__(self, destination_id, ext_events):
         self.destination_id, self.ext_events = destination_id, ext_events
 
+
 class EventDefinition:
     def __init__(self, name, definition_filename):
         self.name, self.definition_filename = name, definition_filename
+
 
 class MetaDefinition:
     def __init__(self, schema_version, fields):
         self.schema_version, self.fields = schema_version, fields
 
+
 class Field:
     def __init__(self, fieldname, allowed_values, required, description):
         self.fieldname, self.allowed_values, self.required, self.description = fieldname, allowed_values, required, description
+
 
 def getVersionIntFromStr(version):
     l = [int(x, 10) for x in version.split('.')]
     l.reverse()
     version = sum(x * (100 ** i) for i, x in enumerate(l))
     return version
+
 
 def checkIfProgramAndEventAllowed(meta_destination_id, meta_ext_event):
     allowed_programs_and_events_filename = "allowed_destination_and_events.json"
@@ -35,7 +51,9 @@ def checkIfProgramAndEventAllowed(meta_destination_id, meta_ext_event):
                 for ext_event in definition.ext_events:
                     if ext_event.name == meta_ext_event:
                         return ext_event.definition_filename
-        raise Exception("Not a recognized combination of meta_destination_id (" + meta_destination_id + ") and meta_ext_event (" + meta_ext_event + ")")
+        raise Exception(
+            "Not a recognized combination of meta_destination_id (" + meta_destination_id + ") and meta_ext_event (" + meta_ext_event + ")")
+
 
 def getSchemaVersionToUse(definitionObj, requested_schema_version):
     if requested_schema_version != None:
@@ -45,17 +63,20 @@ def getSchemaVersionToUse(definitionObj, requested_schema_version):
                 # found requested schema version
                 return fieldDef
             available_schemas.append(str(fieldDef.schema_version))
-        raise Exception("Requested schema version " + requested_schema_version + " not available.  Available schema versions: " + str(available_schemas))
+        raise Exception(
+            "Requested schema version " + requested_schema_version + " not available.  Available schema versions: " + str(
+                available_schemas))
     # provide the oldest found schema
     oldest_available_schema_version_int = None
     oldest_available_schema = None
     for fieldDef in definitionObj:
         schema_version_int = getVersionIntFromStr(fieldDef.schema_version)
-        #print("found schema_version_int = " + str(schema_version_int))
+        # print("found schema_version_int = " + str(schema_version_int))
         if oldest_available_schema == None or schema_version_int < oldest_available_schema_version_int:
             oldest_available_schema_version_int = schema_version_int
             oldest_available_schema = fieldDef
     return oldest_available_schema
+
 
 def checkProgramEventMetadata(program_event_meta_filename, metadata):
     # lookup remaining metadata fields specific to this meta_destination_id and meta_ext_event
@@ -63,6 +84,7 @@ def checkProgramEventMetadata(program_event_meta_filename, metadata):
         metadata_definition = file.read()
         definitionObj = json.loads(metadata_definition, object_hook=lambda d: Namespace(**d))
         checkMetadataAgainstDefinition(definitionObj, metadata)
+
 
 def checkMetadataAgainstDefinition(definitionObj, metadata):
     # check if the schema was provided and if not, default to the oldest schema
@@ -79,46 +101,97 @@ def checkMetadataAgainstDefinition(definitionObj, metadata):
             missing_metadata_fields.append(fieldDef)
         if fieldDef.fieldname in meta_json:
             fieldValue = meta_json[fieldDef.fieldname]
-            if fieldDef.allowed_values != None and len(fieldDef.allowed_values) > 0 and fieldValue not in fieldDef.allowed_values:
-                print(fieldDef.fieldname + " = " + fieldValue + " is not one of the allowed values: " + json.dumps(fieldDef.allowed_values))
+            if fieldDef.allowed_values != None and len(
+                    fieldDef.allowed_values) > 0 and fieldValue not in fieldDef.allowed_values:
+                print(fieldDef.fieldname + " = " + fieldValue + " is not one of the allowed values: " + json.dumps(
+                    fieldDef.allowed_values))
                 validationError = True
     if len(missing_metadata_fields) > 0:
         for fieldDef in missing_metadata_fields:
-            print("Missing required metadata '" + fieldDef.fieldname + "', description = '" + fieldDef.description + "'")
+            print(
+                "Missing required metadata '" + fieldDef.fieldname + "', description = '" + fieldDef.description + "'")
             validationError = True
     if validationError:
         raise Exception("Provided metadata: " + metadata)
-            
-def checkMetadata(metadata):
-    meta_json = json.loads(metadata)
-    min_required_metadata = ["meta_destination_id", "meta_ext_event"]
+
+
+def get_required_metadata(metadata_str):
+    meta_json = json.loads(metadata_str)
     missing_metadata_fields = []
-    for meta_field in min_required_metadata:
-        if not meta_field in meta_json:
-            missing_metadata_fields.append(meta_field)
+
+    for field in required_metadata_fields:
+        if field not in meta_json:
+            missing_metadata_fields.append(field)
+
     if len(missing_metadata_fields) > 0:
-        raise Exception("Missing one or more required metadata fields: " + str(missing_metadata_fields) + ", metadata = " + metadata)
-    meta_destination_id = meta_json["meta_destination_id"]
-    meta_ext_event = meta_json["meta_ext_event"]
+        failure_message = 'Missing one or more required metadata fields: ' + str(missing_metadata_fields)
+
+        dest_id = "not provided"
+        if 'meta_destination_id' not in missing_metadata_fields:
+            dest_id = meta_json['meta_destination_id']
+
+        event_type = "not provided"
+        if 'meta_ext_event' not in missing_metadata_fields:
+            event_type = meta_json['meta_ext_event']
+
+        handle_verification_failure(failure_message, dest_id, event_type)
+
+    return [
+        meta_json['meta_destination_id'],
+        meta_json['meta_ext_event'],
+    ]
+
+
+def handle_verification_failure(message, destination_id, event_type):
+    ps_api_controller = ProcStatController(os.getenv('PS_API_URL'))
+
+    # Create trace for upload
+    upload_id = uuid.uuid4()
+    trace_id, parent_span_id = ps_api_controller.create_upload_trace(upload_id, destination_id, event_type)
+
+    # Start the upload stage metadata verification span
+    trace_id, metadata_verify_span_id \
+        = ps_api_controller.start_span_for_trace(trace_id, parent_span_id, 'metadata-verify')
+    logger.debug(
+        f'Started child span {metadata_verify_span_id} with stage name metadata-verify of parent span {parent_span_id}')
+
+    # TODO: Send report with metadata failure issues.
+
+    # Stop the upload stage metadata verification span
+    ps_api_controller.stop_span_for_trace(trace_id, metadata_verify_span_id)
+    logger.debug(
+        f'Stopped child span {metadata_verify_span_id} with stage name metadata-verify of parent span {parent_span_id} ')
+
+    raise Exception(message)
+
+
+def verify_metadata(metadata):
+    dest_id, event = get_required_metadata(metadata)
+
     # check if the program/event type is on the list of allowed
-    filename = checkIfProgramAndEventAllowed(meta_destination_id, meta_ext_event)
-    if filename != None:
+    filename = checkIfProgramAndEventAllowed(dest_id, event)
+    if filename is not None:
         checkProgramEventMetadata(filename, metadata)
-    
+
+
 def main(argv):
+    log_level = logging.INFO
+    logging.basicConfig(level=log_level)
+
     metadata = ''
-    opts, args = getopt.getopt(argv,"hm:",["metadata="])
+    opts, args = getopt.getopt(argv, "hm:", ["metadata="])
     for opt, arg in opts:
         if opt == '-h':
-            print ('pre-create-bin.py -m <inputfile>')
+            print('pre-create-bin.py -m <inputfile>')
             sys.exit()
         elif opt in ("-m", "--metadata"):
             metadata = arg
     try:
-        checkMetadata(metadata)
+        verify_metadata(metadata)
     except Exception as e:
         print(e)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ tus/file-hooks/metadata-verify/proc_stat_controller.py
@@ -0,0 +1,103 @@
+import requests
+import os
+import logging
+import time
+from requests import Request, Session
+
+MAX_RETRIES = os.getenv("PS_API_MAX_RETRIES") or 6
+
+
+def _handle_trace_response(resp_json):
+    if 'trace_id' not in resp_json or 'span_id' not in resp_json:
+        raise Exception('Invalid PS API response: ' + str(resp_json))
+
+
+def _handle_span_response(resp_json):
+    if 'trace_id' not in resp_json or 'span_id' not in resp_json:
+        raise Exception('Invalid PS API response: ' + str(resp_json))
+
+
+class ProcStatController:
+    def __init__(self, url, delay_s=1):
+        self.url = url
+        self.delay_s = delay_s
+        self.session = Session()
+        self.retry_count = 0
+        self.logger = logging.getLogger(__name__)
+
+    def __del__(self):
+        self.session.close()
+
+    def create_upload_trace(self, upload_id, destination_id, event_type):
+        params = {
+            'uploadId': upload_id,
+            'destinationId': destination_id,
+            'eventType': event_type
+        }
+        req = Request('POST', f'{self.url}/api/trace', params=params)
+        response = self._send_request_with_retry(req.prepare())
+
+        resp_json = response.json()
+        _handle_trace_response(resp_json)
+
+        return resp_json['trace_id'], resp_json['span_id']
+
+    def get_trace_by_upload_id(self, upload_id):
+        req = Request('GET', f'{self.url}/api/trace/uploadId/{upload_id}')
+        response = self._send_request_with_retry(req.prepare())
+
+        resp_json = response.json()
+        _handle_trace_response(resp_json)
+
+        return resp_json['trace_id'], resp_json['span_id']
+
+    def get_span_by_upload_id(self, upload_id, stage_name):
+        params = {
+            "uploadId": upload_id,
+            "stageName": stage_name
+        }
+        req = Request('GET', f'{self.url}/api/trace/span', params=params)
+        response = self._send_request_with_retry(req.prepare())
+
+        resp_json = response.json()
+        _handle_span_response(resp_json)
+
+        return resp_json
+
+    def start_span_for_trace(self, trace_id, parent_span_id, stage_name):
+        params = {
+            "stageName": stage_name,
+        }
+        response = requests.put(f'{self.url}/api/trace/startSpan/{trace_id}/{parent_span_id}', params=params)
+        response.raise_for_status()
+
+        resp_json = response.json()
+
+        _handle_trace_response(resp_json)
+
+        return resp_json['trace_id'], resp_json['span_id']
+
+    def stop_span_for_trace(self, trace_id, parent_span_id):
+        req = Request('PUT', f'{self.url}/api/trace/stopSpan/{trace_id}/{parent_span_id}')
+        self._send_request_with_retry(req.prepare())
+
+    def _send_request_with_retry(self, req):
+        # Resetting the retry count.
+        self.retry_count = 0
+
+        while self.retry_count < MAX_RETRIES:
+            try:
+                resp = self.session.send(req)
+                resp.raise_for_status()
+
+                if resp.ok:
+                    # Request was handled successfully, return and don't send any more requests.
+                    return resp
+            except requests.exceptions.RequestException as e:
+                self.logger.warning(f"Error sending request to PS API after attempt {self.retry_count}.  Reason: {e}")
+                self.retry_count = self.retry_count + 1
+
+                # Waiting 2 second before trying again.
+                time.sleep(self.delay_s)
+
+        raise Exception(f"Unable to send successful request to PS API after {MAX_RETRIES} attempts.")
