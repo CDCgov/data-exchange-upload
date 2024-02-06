5e89fb43d733c1a92b60449215636026d15675bc
rohitpanwar <rohitpanwar@users.noreply.github.com>
2024-02-06 17:38:38 -0500

    UPLOAD-967: metadata verification success span (#218)
    * create metadata verify span using PS API
    
    * adding json paylod for metadata verify span
    
    * uploading metadata json using reports endopoint
    
    * addressing feedback comments
    
    * removing ast dependency
    
    * added unit tests
    
    * commenting out the failing assertion temporarily
    
    * adding retires
    
    * fixing some python bugs
    
    ---------
    
    Co-authored-by: Rohit Panwar <rohitpanwar@rohits-mac-mini.lan>
    Co-authored-by: tma5 <tma5@cdc.gov>

--- tus/file-hooks/post-create/proc_stat_controller.py
+++ tus/file-hooks/post-create/proc_stat_controller.py
@@ -1,14 +1,22 @@
 import requests
+import os
+import logging
+import time
+from requests import Request, Session
 
+MAX_RETRIES = os.getenv("PS_API_MAX_RETRIES") or 6
 
 def _handle_trace_response(resp_json):
     if 'trace_id' not in resp_json or 'span_id' not in resp_json:
         raise Exception('Invalid PS API response: ' + str(resp_json))
 
-
 class ProcStatController:
-    def __init__(self, url):
+    def __init__(self, url, delay_s=1):
         self.url = url
+        self.delay_s = delay_s
+        self.session = Session()
+        self.retry_count = 0
+        self.logger = logging.getLogger(__name__)
 
     def create_upload_trace(self, upload_id, destination_id, event_type):
         params = {
@@ -16,8 +24,9 @@ class ProcStatController:
             'destinationId': destination_id,
             'eventType': event_type
         }
-        response = requests.post(f'{self.url}/api/trace', params=params)
-        response.raise_for_status()
+
+        request = Request('POST', f'{self.url}/api/trace', params=params)
+        response = self._send_request_with_retry(request.prepare())
 
         resp_json = response.json()
 
@@ -29,11 +38,47 @@ class ProcStatController:
         params = {
             "stageName": stage_name,
         }
-        response = requests.put(f'{self.url}/api/trace/startSpan/{trace_id}/{parent_span_id}', params=params)
-        response.raise_for_status()
+        
+        request = Request('PUT', f'{self.url}/api/trace/startSpan/{trace_id}/{parent_span_id}', params=params)
+        response = self._send_request_with_retry(request.prepare())
 
         resp_json = response.json()
 
         _handle_trace_response(resp_json)
 
         return resp_json['trace_id'], resp_json['span_id']
+
+    def create_report_json(self, upload_id, destination_id, event_type, stage_name, json_payload):        
+        params = {
+            'destinationId': destination_id,
+            'eventType': event_type,
+            'stageName': stage_name
+        }
+
+        request = Request('PUT', f'{self.url}/api/report/json/uploadId/{upload_id}', params=params, json=json_payload)
+        self._send_request_with_retry(request.prepare())
+        
+    def stop_span_for_trace(self, trace_id, span_id):
+        req = Request('PUT', f'{self.url}/api/trace/stopSpan/{trace_id}/{span_id}')
+        self._send_request_with_retry(req.prepare())
+
+    def _send_request_with_retry(self, req):
+        # Resetting the retry count.
+        self.retry_count = 0
+
+        while self.retry_count < MAX_RETRIES:
+            try:
+                resp = self.session.send(req)
+                resp.raise_for_status()
+
+                if resp.ok:
+                    # Request was handled successfully, return and don't send any more requests.
+                    return resp
+            except requests.exceptions.RequestException as e:
+                self.logger.warning(f"Error sending request to PS API after attempt {self.retry_count}.  Reason: {e}")
+                self.retry_count = self.retry_count + 1
+
+                # Waiting 2 second before trying again.
+                time.sleep(self.delay_s)
+
+        raise Exception(f"Unable to send successful request to PS API after {MAX_RETRIES} attempts.")
--- tus/file-hooks/post-create/test/test_post_create.py
+++ tus/file-hooks/post-create/test/test_post_create.py
@@ -3,43 +3,64 @@ import sys, os
 from unittest.mock import patch
 
 from post_create_bin import get_required_metadata
+from post_create_bin import get_filename_from_metadata
 from post_create_bin import post_create
 
 from proc_stat_controller import ProcStatController
 
+
 class TestPostCreateMethods(unittest.TestCase):
-  def test_should_get_required_metadata(self):
-    valid_metadata_str = """
-      {
-        "meta_ext_event": "test_event",
-        "meta_destination_id": "1234"
-      }
-    """
+  def test_get_required_metadata(self):
+    # Test data
+    metadata_json_dict = {'meta_destination_id': '123', 'meta_ext_event': '456'}
 
-    dest, event = get_required_metadata(valid_metadata_str)
-    self.assertEqual(dest, "1234")
-    self.assertEqual(event, "test_event")
-  
+    # Call the function to be tested
+    result = get_required_metadata(metadata_json_dict)
+
+    # Assert the result
+    self.assertEqual(result, ['123', '456'])
+
   def test_should_raise_error_if_required_metadata_not_provided(self):
-    metadata = """
-      {
-          "meta_ext_event":"456"
-      }
-    """
+    # Test data with missing required fields
+    metadata_json_dict = {'other_field': 'value'}
+
+    # Call the function and assert that it raises an exception
     with self.assertRaises(Exception) as context:
-      get_required_metadata(metadata)
-      
-    self.assertIn('Missing one or more required metadata fields', str(context.exception))
+      get_required_metadata(metadata_json_dict)
 
-  @patch.object(ProcStatController, 'create_upload_trace', return_value=("test_trace", "test_span"))
-  @patch.object(ProcStatController, 'start_span_for_trace')
-  def test_should_send_create_trace_request(self, create_upload_trace_mock, start_span_for_trace_mock):
-    create_upload_trace_mock.return_value = ("test_trace", "test_span")
+    # Assert the exception message
+    self.assertTrue('Missing one or more required metadata fields' in str(context.exception))
 
-    post_create("1234", "test_event", "5678")
+  def test_get_filename_from_metadata(self):
+    # Test data
+    metadata_json_dict = {'filename': 'example.txt', 'original_filename': 'example_original.txt'}
 
+    # Call the function to be tested
+    result = get_filename_from_metadata(metadata_json_dict)
+
+    # Assert the result
+    self.assertEqual(result, 'example.txt')
+  
+  @patch.object(ProcStatController, 'create_upload_trace', return_value=("trace_id", "parent_span_id"))
+  @patch.object(ProcStatController, 'start_span_for_trace', return_value=("trace_id", "metadata_verify_span_id"))
+  @patch.object(ProcStatController, 'create_report_json')
+  @patch.object(ProcStatController, 'stop_span_for_trace')
+  def test_should_send_create_trace_request(self, create_upload_trace_mock, start_span_for_trace_mock, create_report_json_mock, stop_span_for_trace_mock):
+    
+    # Test data
+    dest = 'destination'
+    event = 'some_event'
+    metadata_json_dict = {'meta_destination_id': 'destination', 'meta_ext_event': 'some_event'}
+    tguid = 'some_tguid'
+    
+    # Call the function
+    post_create(dest, event, metadata_json_dict, tguid)
+
+    # Assert the result
     self.assertEqual(create_upload_trace_mock.call_count, 1)
     self.assertEqual(start_span_for_trace_mock.call_count, 1)
+    # self.assertEqual(create_report_json_mock.call_count, 1)
+    self.assertEqual(stop_span_for_trace_mock.call_count, 1)
 
 if __name__ == "__main__":
   unittest.main()
