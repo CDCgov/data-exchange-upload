b82c6d8ce4571aca7c678038910818c179cbc513
tma5 <tma5@cdc.gov>
2024-02-09 11:15:54 -0500

    tests for required fields

--- tests/integration/kotlin/src/test/kotlin/MetadataVerify.kt
+++ tests/integration/kotlin/src/test/kotlin/MetadataVerify.kt
@@ -1,4 +1,5 @@
 import auth.AuthClient
+import io.tus.java.client.ProtocolException
 import org.testng.Assert
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
@@ -11,7 +12,9 @@ import java.io.File
 class MetadataVerify {
     private val TEST_DESTINATION = "dextesting"
     private val TEST_EVENT = "testevent1"
-    private var authClient = AuthClient(Env.UPLOAD_URL)
+    // TODO: Handle test file not found.
+    private val testFile = File(MetadataVerify::class.java.getResource("10KB-test-file").file)
+    private val authClient = AuthClient(Env.UPLOAD_URL)
     private lateinit var uploadClient: UploadClient
 
     @BeforeClass()
@@ -22,16 +25,49 @@ class MetadataVerify {
 
     @Test()
     fun shouldUploadFileGivenRequiredMetadata() {
-        // TODO: Handle test file not found.
-        val file = File(MetadataVerify::class.java.getResource("10KB-test-file").file)
         val metadata = hashMapOf(
-            "filename" to file.name,
+            "filename" to testFile.name,
             "meta_destination_id" to TEST_DESTINATION,
             "meta_ext_event" to TEST_EVENT,
             "meta_ext_source" to "INTEGRATION-TEST"
         ) as HashMap<String, String>
 
-        val uploadId = uploadClient.uploadFile(file, metadata)
+        val uploadId = uploadClient.uploadFile(testFile, metadata)
+        Assert.assertNotNull(uploadId)
+    }
+
+    @Test(expectedExceptions = [ProtocolException::class])
+    fun shouldReturnErrorWhenDestinationIDNotProvided() {
+        val metadata = hashMapOf(
+            "filename" to testFile.name,
+            "meta_ext_event" to TEST_EVENT,
+            "meta_ext_source" to "INTEGRATION-TEST"
+        ) as HashMap<String, String>
+
+        uploadClient.uploadFile(testFile, metadata)
+    }
+
+    @Test(expectedExceptions = [ProtocolException::class])
+    fun shouldReturnErrorWhenEventNotProvided() {
+        val metadata = hashMapOf(
+            "filename" to testFile.name,
+            "meta_destination_id" to TEST_DESTINATION,
+            "meta_ext_source" to "INTEGRATION-TEST"
+        ) as HashMap<String, String>
+
+        val uploadId = uploadClient.uploadFile(testFile, metadata)
+        Assert.assertNotNull(uploadId)
+    }
+
+    @Test(expectedExceptions = [ProtocolException::class])
+    fun shouldReturnErrorWhenFilenameNotProvided() {
+        val metadata = hashMapOf(
+            "meta_destination_id" to TEST_DESTINATION,
+            "meta_ext_event" to TEST_EVENT,
+            "meta_ext_source" to "INTEGRATION-TEST"
+        )
+
+        val uploadId = uploadClient.uploadFile(testFile, metadata)
         Assert.assertNotNull(uploadId)
     }
 }
