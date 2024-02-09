b733dceb7c1af385d92ed1fdac72aac86433ddea
tma5 <tma5@cdc.gov>
2024-02-09 16:06:22 -0500

    splitting tests into two groups

--- tests/integration/kotlin/src/test/kotlin/util/Constants.kt
+++ tests/integration/kotlin/src/test/kotlin/util/Constants.kt
@@ -5,4 +5,11 @@ class Constants {
         val TEST_DESTINATION = "dextesting"
         val TEST_EVENT = "testevent1"
     }
+
+    class Groups {
+        companion object {
+            const val PROC_STAT_METADATA_VERIFY_HAPPY_PATH = "proc-stat-metadata-verify-happy-path"
+            const val PROC_STAT_UPLOAD_STATUS_HAPPY_PATH = "proc-stat-upload-status-happy-path"
+        }
+    }
 }
--- tests/integration/kotlin/src/test/kotlin/util/Groups.kt
+++
@@ -1,7 +0,0 @@
-package util
-
-class Groups {
-    companion object {
-        const val PROC_STAT_TRACE_HAPPY_PATH = "proc-stat-trace-happy-path"
-    }
-}
