3057688eec1d08d5fe0735c9447986373df4feea
tma5 <tma5@cdc.gov>
2024-02-09 09:09:57 -0500

    uploads a file given required metadata

--- tests/integration/kotlin/src/test/kotlin/MetadataVerify.kt
+++ tests/integration/kotlin/src/test/kotlin/MetadataVerify.kt
@@ -4,10 +4,13 @@ import org.testng.annotations.BeforeClass
 import org.testng.annotations.Test
 import tus.UploadClient
 import util.Env
+import java.io.File
 
 
 @Test()
 class MetadataVerify {
+    private val TEST_DESTINATION = "dextesting"
+    private val TEST_EVENT = "testevent1"
     private var authClient = AuthClient(Env.UPLOAD_URL)
     private lateinit var uploadClient: UploadClient
 
@@ -18,9 +21,18 @@ class MetadataVerify {
     }
 
     @Test()
-    fun test() {
-        uploadClient.uploadFile()
-        Assert.assertEquals(1, 1)
+    fun shouldUploadFileGivenRequiredMetadata() {
+        // TODO: Handle test file not found.
+        val file = File(MetadataVerify::class.java.getResource("10KB-test-file").file)
+        val metadata = hashMapOf(
+            "filename" to file.name,
+            "meta_destination_id" to TEST_DESTINATION,
+            "meta_ext_event" to TEST_EVENT,
+            "meta_ext_source" to "INTEGRATION-TEST"
+        ) as HashMap<String, String>
+
+        val uploadId = uploadClient.uploadFile(file, metadata)
+        Assert.assertNotNull(uploadId)
     }
 }
 
