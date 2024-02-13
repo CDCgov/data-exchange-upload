d09a8255766685cf710be55cff7648d4911c1966
Gebre <USZ7-SU@cdc.gov>
2024-02-13 08:14:27 -0500

    upload-969 address comments

--- upload-processor/BulkFileUploadFunctionApp/Model/HealthCheckResult.cs
+++ upload-processor/BulkFileUploadFunctionApp/Model/HealthCheckResult.cs
@@ -7,12 +7,12 @@ namespace BulkFileUploadFunctionApp.Model
         public string Status { get; set; }
         public string HealthIssues { get; set; }
 
-    // Constructor for easily setting properties upon creation
-    public HealthCheckResult(string service, string status, string healthIssues = "")
-    {
-        Service = service;
-        Status = status;
-        HealthIssues = healthIssues;
-    }
+        // Constructor for easily setting properties upon creation
+        public HealthCheckResult(string service, string status, string healthIssues = "")
+        {
+            Service = service ?? throw new ArgumentNullException(nameof(service), "Service cannot be null.");
+            Status = status ?? throw new ArgumentNullException(nameof(status), "Status cannot be null.");
+            HealthIssues = healthIssues;
+        }
     }
 }
--- upload-processor/BulkFileUploadFunctionApp/Program.cs
+++ upload-processor/BulkFileUploadFunctionApp/Program.cs
@@ -33,11 +33,7 @@ var host = new HostBuilder()
         services.AddSingleton<IBlobServiceClientFactory, BlobServiceClientFactoryImpl>();
 
         // Registers an implementation for the IEnvironmentVariableProvider interface to be resolved as a singleton.
-        services.AddSingleton<IEnvironmentVariableProvider, EnvironmentVariableProviderImpl>();
-
-        // Registers an implementation for the IStorageContentReader interface to be resolved as a singleton.
-        services.AddSingleton<IStorageContentReader, StorageContentReader>();
-       
+        services.AddSingleton<IEnvironmentVariableProvider, EnvironmentVariableProviderImpl>();       
     })
     .Build();
 
