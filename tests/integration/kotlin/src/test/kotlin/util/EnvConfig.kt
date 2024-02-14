e7e1c895f379b724bdece30ed3f811c7a867a3e3
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2024-02-14 11:53:05 -0500

    check if file is null (#230)

--- tests/integration/kotlin/src/test/kotlin/util/EnvConfig.kt
+++ tests/integration/kotlin/src/test/kotlin/util/EnvConfig.kt
@@ -5,12 +5,13 @@ import java.util.Properties
 
 class EnvConfig {
     companion object {
-        private val properties = Properties().apply {
+        private val propFile = File("local.properties")
+        private val properties = if (propFile.exists()) Properties().apply {
             load(File("local.properties").inputStream())
-        }
-        val UPLOAD_URL: String = properties.getProperty("upload.url") ?: System.getenv("UPLOAD_URL")
-        val PROC_STAT_URL: String = properties.getProperty("ps.api.url") ?: System.getenv("PS_API_URL")
-        val SAMS_USERNAME: String = properties.getProperty("sams.username") ?: System.getenv("SAMS_USERNAME")
-        val SAMS_PASSWORD: String = properties.getProperty("sams.password") ?: System.getenv("SAMS_PASSWORD")
+        } else null
+        val UPLOAD_URL: String = properties?.getProperty("upload.url") ?: System.getenv("UPLOAD_URL")
+        val PROC_STAT_URL: String = properties?.getProperty("ps.api.url") ?: System.getenv("PS_API_URL")
+        val SAMS_USERNAME: String = properties?.getProperty("sams.username") ?: System.getenv("SAMS_USERNAME")
+        val SAMS_PASSWORD: String = properties?.getProperty("sams.password") ?: System.getenv("SAMS_PASSWORD")
     }
 }
