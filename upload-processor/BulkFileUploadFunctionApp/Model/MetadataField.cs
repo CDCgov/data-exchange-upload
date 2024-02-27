fc9d63269f120495dcf75082523958c5c71ad8da
tma5 <tma5@cdc.gov>
2024-02-27 09:48:10 -0500

    remove setter for records

--- upload-processor/BulkFileUploadFunctionApp/Model/MetadataField.cs
+++ upload-processor/BulkFileUploadFunctionApp/Model/MetadataField.cs
@@ -4,8 +4,8 @@ namespace BulkFileUploadFunctionApp.Model
 {
     public record MetadataField
     {
-        [JsonProperty("field_name")] public string? FieldName { get; set; }
-        [JsonProperty("compat_field_name")] public string? CompatFieldName { get; set; }
-        [JsonProperty("default_value")] public string? DefaultValue { get; set; }
+        [JsonProperty("field_name")] public string? FieldName { get; init; }
+        [JsonProperty("compat_field_name")] public string? CompatFieldName { get; init; }
+        [JsonProperty("default_value")] public string? DefaultValue { get; init; }
     }
 }
--- upload-processor/BulkFileUploadFunctionApp/Model/UploadConfig.cs
+++ upload-processor/BulkFileUploadFunctionApp/Model/UploadConfig.cs
@@ -4,10 +4,10 @@ namespace BulkFileUploadFunctionApp.Model
 {
     public record UploadConfig
     {
-        [JsonProperty("filename_suffix")] public string? FilenameSuffix { get; set; }
-        [JsonProperty("folder_structure")] public string? FolderStructure { get; set; }
-        [JsonProperty("fixed_folder_path")] public string? FixedFolderPath { get; set; }
-        [JsonProperty("metadata_fields")] public List<MetadataField>? MetadataFields { get; set; }
+        [JsonProperty("filename_suffix")] public string? FilenameSuffix { get; init; }
+        [JsonProperty("folder_structure")] public string? FolderStructure { get; init; }
+        [JsonProperty("fixed_folder_path")] public string? FixedFolderPath { get; init; }
+        [JsonProperty("metadata_fields")] public List<MetadataField>? MetadataFields { get; init; }
 
         public static readonly UploadConfig Default = new UploadConfig()
         {
