bc8cdd1c0d9943bd39e2c92251b75917a4fcdf5e
Chase Farmer <98106992+cfarmer-fearless@users.noreply.github.com>
2024-02-27 16:55:26 -0500

    Upload 924/v2 metadata hydration (#240)
    * v2 metadata mapping config file
    
    * data prod id field for aims celr
    
    * fix typo in jurisdiction
    
    * reporting jurisdiction for daart
    
    * v2 upload configs
    
    * WIP new upload configs but failing to deserialize
    
    * debugging and cleanup
    
    * fields for data stream and route
    
    * remove setter for records
    
    * add comment for filename metadata
    
    * version field and config restructure

---
+++ upload-processor/BulkFileUploadFunctionApp/Model/MetadataVersion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkFileUploadFunctionApp.Model
+{
+    public enum MetadataVersion
+    {
+        V1 = 1,
+        V2 = 2
+    }
+}
