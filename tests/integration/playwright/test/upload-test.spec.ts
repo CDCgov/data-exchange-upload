5a8024a18732d1d91a3e06fbdaf54b1869cf1e4c
Gebre <USZ7-SU@cdc.gov>
2024-02-29 10:38:31 -0500

    upload-1017 fix unexpected empty object pattern

--- tests/integration/playwright/test/upload-test.spec.ts
+++ tests/integration/playwright/test/upload-test.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 
 import { start } from '../index'; // Assuming start and getTraceResponse are exported
 
-test('upload and get trace response', async ({}) => {
+test('upload and get trace response', async () => {
   
   await start(); // Make sure this function is adapted to fit within a testing context if necessary
 
