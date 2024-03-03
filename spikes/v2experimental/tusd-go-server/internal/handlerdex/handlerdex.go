4f4e34ac673cfe3448a311948902b25cd21eb4cf
Cosmin Potocean <cosmycx@gmail.com>
2024-03-03 11:08:16 -0500

    v2 go minor clean up root index

--- spikes/v2experimental/tusd-go-server/internal/handlerdex/handlerdex.go
+++ spikes/v2experimental/tusd-go-server/internal/handlerdex/handlerdex.go
@@ -19,18 +19,20 @@ func New(flags flags.Flags, config config.Config) (*HandlerDex, error) {
 func (hd HandlerDex) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
     switch r.URL.Path {
-    case "/":
-        hd.root(w, r)
 
-    case "/health":
-        hd.health(w, r)
+        case "/":
+            hd.root(w, r)
 
-	case "/version":
-        hd.version(w, r)
+        case "/health":
+            hd.health(w, r)
 
-	// all non-specific routes
-    default:
-        http.NotFound(w, r)
-    } // .default 
+        case "/version":
+            hd.version(w, r)
+
+        // all other non-specified routes
+        default:
+            http.NotFound(w, r)
+
+    } // .switch 
 
 } // .ServeHTTP
