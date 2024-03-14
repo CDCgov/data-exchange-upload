ce186db3799dab9c67d5fe0a0a3bfeb0bfd3dcc0
ttaliafer <ted.taliaferro@mstro.ai>
2024-03-14 14:32:16 -0400

    Refactor HealthCheckResponse and StorageBlobCreatedEvent classes

--- upload-processor/BulkFileUploadFunctionApp/Model/StorageBlobCreatedEvent.cs
+++ upload-processor/BulkFileUploadFunctionApp/Model/StorageBlobCreatedEvent.cs
@@ -5,21 +5,23 @@ namespace BulkFileUploadFunctionApp.Model
 {
     public class StorageBlobCreatedEventData
     {
+        [JsonPropertyName("url")]
         public string? Url { get; set; }
     }
 
     public class StorageBlobCreatedEvent
     {
+        [JsonPropertyName("id")]
         public string? Id { get; set; }
-
+        [JsonPropertyName("topic")]
         public string? Topic { get; set; }
-
+        [JsonPropertyName("subject")]
         public string? Subject { get; set; }
-
+        [JsonPropertyName("eventType")]
         public string? EventType { get; set; }
-
+        [JsonPropertyName("eventTime")]
         public DateTime EventTime { get; set; }
-
+        [JsonPropertyName("data")]
         public StorageBlobCreatedEventData? Data { get; set; }
     }
 }
