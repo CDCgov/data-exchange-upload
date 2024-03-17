9f4df36d8bb09d0883142a6454c2424f3cc71553
Cosmin Potocean <cosmycx@gmail.com>
2024-03-17 13:31:19 -0400

    v2 go copier changes

--- spikes/v2experimental/tusd-go-server/internal/storecopier/onuploadcomplete.go
+++ spikes/v2experimental/tusd-go-server/internal/storecopier/onuploadcomplete.go
@@ -1,34 +1,60 @@
 package storecopier
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/appconfig"
 	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/cliflags"
 	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/metadatav1"
-	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/storecopierlocal"
+	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/storelocal"
 	tusd "github.com/tus/tusd/v2/pkg/handler"
 ) // .import
 
 // OnUploadComplete gets notification on a tusd upload complete and makes the store copies necessary per config
 func OnUploadComplete(flags cliflags.Flags, appConfig appconfig.AppConfig, allUploadConfig metadatav1.AllUploadConfigs, eventUploadComplete tusd.HookEvent) error {
 
-	// LOCAL
+	evInfo := FromTusToDstEvent{
+		AppConfig:           appConfig,
+		AllUploadConfig:     allUploadConfig,
+		EventUploadComplete: eventUploadComplete,
+	} // .eventInfo
+
+	
+	// ------------------------------------------------------------------
+	// ENV_LOCAL
+	// ------------------------------------------------------------------
 	if flags.Environment == cliflags.ENV_LOCAL {
 
-		// making only one local copy as concept from tusd raw to a local folder
-		lc := storecopierlocal.FromTusToDstCopier{
-			AppConfig:           appConfig,
-			AllUploadConfig:     allUploadConfig,
-			EventUploadComplete: eventUploadComplete,
-		} // .lc
+		sl := storelocal.CopierLocal{
 
-		err := lc.CopyTusSrcToDst()
+			SrcFileName: evInfo.EventUploadComplete.Upload.ID,
+			SrcFolder : evInfo.AppConfig.LocalFolderUploadsTus,
+		
+			// TODO config destination per respective upload config
+			// TODO: adding file ticks, change per config
+			// _ = cl.AllUploadConfig // TODO
+		
+			DstFolder: evInfo.AppConfig.LocalFolderUploadsA,
+			DstFileName: evInfo.EventUploadComplete.Upload.MetaData["filename"] + "_" + strconv.FormatInt(time.Now().UnixNano(), 10),
+
+		}// .cl
+
+		err := sl.CopyTusSrcToDst()
 		if err != nil {
 			return err
 		} // .err
 
-	} // .LOCAL
+	} // .ENV_LOCAL
 
-	// AZURE
+		
+	// ------------------------------------------------------------------
+	// ENV_AZURE
+	// ------------------------------------------------------------------
+	if flags.Environment == cliflags.ENV_AZURE {
+
+
+	}// .ENV_AZURE
 
 	return nil
 } // .OnUploadComplete
--- spikes/v2experimental/tusd-go-server/internal/storecopier/storecopier.go
+++ spikes/v2experimental/tusd-go-server/internal/storecopier/storecopier.go
@@ -1,13 +1,21 @@
 package storecopier
 
-// storeCopier interface across stores: local, azure, and aws.
-type StoreCopier interface {
+// StoreTusCopier interface across stores: local, azure, and aws.
+type StoreTusCopier interface {
 
 	// CopyTusSrcToDst copies a file with metadata from .info into the destination file
 	// cli flags based for local or cloud copies
 	CopyTusSrcToDst() error
+} // .StoreCopier
+
+
+// TODO:, this will be the router also
+
+// StoreCopier interface across stores: local, azure, and aws.
+type StoreCopier interface {
 
 	// TODO:
 	// CopySrcToDst copies a file from Src location to Dst location
 	CopySrcToDst() error
 } // .StoreCopier
+
