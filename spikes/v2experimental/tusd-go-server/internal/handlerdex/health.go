607b0452aa7c28f0ed5b649e89d531bdeef8df54
Cosmin Potocean <cosmycx@gmail.com>
2024-03-17 16:08:00 -0400

    v2 go health for azure store

--- spikes/v2experimental/tusd-go-server/internal/handlerdex/health.go
+++ spikes/v2experimental/tusd-go-server/internal/handlerdex/health.go
@@ -4,13 +4,22 @@ import (
 	"encoding/json"
 	"net/http"
 	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
+	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/cliflags"
 ) // .import
 
 type HealthResp struct { // TODO: line up with DEX other products and apps
 
 	RootResp        // Embedding rootResp, TODO: maybe this is not needed
 	Health   string `json:"health"` // TODO: line-up
+	Status   string `json:"status"`
 
+	// TODO: temp for dev
+	ErrAzIdentity string `json:"err_az_identity"`
+	ErrAzBlob     string `json:"err_az_blob"`
 } // .Health
 
 // health responds to /health endpoint with the health of the app
@@ -18,14 +27,40 @@ type HealthResp struct { // TODO: line up with DEX other products and apps
 // TODO: check the dependencies such as storages
 func (hd *HandlerDex) health(w http.ResponseWriter, r *http.Request) {
 
+	status := "ok"
+
+	// check storage dependency:
+	var errAzIdentity, errAzBlob error
+	var credential azcore.TokenCredential
+
+	if hd.cliFlags.Environment == cliflags.ENV_AZURE {
+
+		// TODO: replace <storage-account-name> with your actual storage account name
+		url := hd.appConfig.AzContainerEndpoint
+
+		credential, errAzIdentity = azidentity.NewDefaultAzureCredential(nil)
+
+		_, errAzBlob = azblob.NewClient(url, credential, nil)
+
+	} // .if
+
+	if errAzIdentity != nil || errAzBlob != nil {
+		status = "bad"
+	} // .if
+
 	jsonResp, err := json.Marshal(HealthResp{
 		RootResp: RootResp{
-			System:     hd.appConfig.System,
-			DexProduct: hd.appConfig.DexProduct,
-			DexApp:     hd.appConfig.DexApp,
-			ServerTime: time.Now().Format(time.RFC3339),
+			System:      hd.appConfig.System,
+			DexProduct:  hd.appConfig.DexProduct,
+			DexApp:      hd.appConfig.DexApp,
+			ServerTime:  time.Now().Format(time.RFC3339),
+			Environment: hd.cliFlags.Environment,
 		},
 		Health: "All Good",
+		Status: status,
+
+		ErrAzIdentity: errAzIdentity.Error(),
+		ErrAzBlob:     errAzBlob.Error(),
 	}) // .jsonResp
 	if err != nil {
 		errMsg := "error marshal json for health response"
