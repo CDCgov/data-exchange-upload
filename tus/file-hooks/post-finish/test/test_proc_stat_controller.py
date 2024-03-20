c66702ead046e6da5d8f3136b0c481c42bea482a
David Gage <whytheplatypus@gmail.com>
2024-03-20 13:31:26 -0400

    Update tests for proc_stat_controller
    to reflect that a non-recoverable error should not be retried

--- tus/file-hooks/post-finish/test/test_proc_stat_controller.py
+++ tus/file-hooks/post-finish/test/test_proc_stat_controller.py
@@ -9,7 +9,7 @@ class TestProcStatController(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             controller.get_trace_by_upload_id(None)
 
-        self.assertEqual(controller.retry_count, 6)
+        self.assertEqual(controller.retry_count, 1)
 
     def test_should_retry_get_span_when_upload_id_invalid(self):
         controller = ProcStatController('http://dummy', .01)
@@ -17,7 +17,7 @@ class TestProcStatController(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             controller.get_span_by_upload_id(None, 'dummy')
 
-        self.assertEqual(controller.retry_count, 6)
+        self.assertEqual(controller.retry_count, 1)
 
     def test_should_retry_stop_span_when_input_invalid(self):
         controller = ProcStatController('http://dummy', .01)
@@ -25,4 +25,4 @@ class TestProcStatController(unittest.TestCase):
         with self.assertRaises(Exception) as context:
             controller.stop_span_for_trace(None, None)
 
-        self.assertEqual(controller.retry_count, 6)
+        self.assertEqual(controller.retry_count, 1)
