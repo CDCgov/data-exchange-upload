cac52854c94e336088479a920ce35d0d60ed80f3
tma5 <tma5@cdc.gov>
2024-03-22 11:49:27 -0400

    fix merge changes and run all tests

--- tests/integration/kotlin/src/test/kotlin/util/Constants.kt
+++ tests/integration/kotlin/src/test/kotlin/util/Constants.kt
@@ -4,6 +4,10 @@ class Constants {
     companion object {
         const val TEST_DESTINATION = "dextesting"
         const val TEST_EVENT = "testevent1"
+        const val BULK_UPLOAD_CONTAINER_NAME = "bulkuploads"
+        const val EDAV_UPLOAD_CONTAINER_NAME = "upload"
+        const val ROUTING_UPLOAD_CONTAINER_NAME = "routeingress"
+        const val TUS_PREFIX_DIRECTORY_NAME = "tus-prefix"
     }
 
     class Groups {
@@ -12,6 +16,7 @@ class Constants {
             const val PROC_STAT = "proc-stat"
             const val PROC_STAT_TRACE = "proc-stat-trace"
             const val PROC_STAT_REPORT = "proc-stat-report"
+            const val FILE_COPY = "file-copy"
         }
     }
 }
--- tests/integration/kotlin/src/test/kotlin/util/Metadata.kt
+++ tests/integration/kotlin/src/test/kotlin/util/Metadata.kt
@@ -1,11 +1,22 @@
 package util
 
+import org.joda.time.DateTime
+import java.io.File
 import java.io.FileNotFoundException
 import java.util.*
 import kotlin.collections.HashMap
 
 class Metadata {
     companion object {
+        fun generateRequiredMetadataForFile(file: File): HashMap<String, String> {
+            return hashMapOf(
+                "filename" to file.name,
+                "meta_destination_id" to Constants.TEST_DESTINATION,
+                "meta_ext_event" to Constants.TEST_EVENT,
+                "meta_ext_source" to "INTEGRATION-TEST"
+            )
+        }
+
         fun convertPropertiesToMetadataMap(propertiesFilePath: String): HashMap<String, String> {
             val metadata = HashMap<String, String>()
             val properties = Properties()
@@ -23,6 +34,13 @@ class Metadata {
             return metadata
         }
 
+        // Using Calendar due to deprecation of Date.
+        fun getFilePrefixByDate(date: DateTime, useCaseDir: String): String {
+            // Pad date numbers with 0.
+            val month = if (date.monthOfYear < 10) "0${date.monthOfYear}" else "${date.monthOfYear}"
+            val day = if (date.dayOfMonth < 10) "0${date.dayOfMonth}" else "${date.dayOfMonth}"
+            return "$useCaseDir/${date.year}/$month/$day"
+        }
     }
 }
 
