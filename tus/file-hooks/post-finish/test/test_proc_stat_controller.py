2ce5cd4dc0ffe5cd9fff15aa1667888a55939c68
David Gage <whytheplatypus@gmail.com>
2024-03-25 11:58:39 -0400

    Add tests for recoverable errors

--- tus/file-hooks/post-finish/test/test_proc_stat_controller.py
+++ tus/file-hooks/post-finish/test/test_proc_stat_controller.py
@@ -1,5 +1,6 @@
 import unittest
 from proc_stat_controller import ProcStatController
+from httmock import all_requests, HTTMock
 
 
 class TestProcStatController(unittest.TestCase):
@@ -26,3 +27,74 @@ class TestProcStatController(unittest.TestCase):
             controller.stop_span_for_trace(None, None)
 
         self.assertEqual(controller.retry_count, 1)
+
+    def test_should_retry_429(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with HTTMock(response_retry_limit):
+            with self.assertRaises(Exception) as context:
+                controller.stop_span_for_trace(None, None)
+
+        self.assertEqual(controller.retry_count, 6)
+
+    def test_should_retry_429_with_after(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with HTTMock(response_retry_limit_with_after):
+            with self.assertRaises(Exception) as context:
+                controller.stop_span_for_trace(None, None)
+
+        self.assertEqual(controller.retry_count, 6)
+
+    def test_should_retry_503(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with HTTMock(response_retry_unavailable):
+            with self.assertRaises(Exception) as context:
+                controller.stop_span_for_trace(None, None)
+
+        self.assertEqual(controller.retry_count, 6)
+
+    def test_should_retry_503_with_after(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with HTTMock(response_retry_unavailable_with_after):
+            with self.assertRaises(Exception) as context:
+                controller.stop_span_for_trace(None, None)
+
+        self.assertEqual(controller.retry_count, 6)
+
+    def test_should_succeed(self):
+        controller = ProcStatController('http://dummy', .01)
+
+        with HTTMock(response_success):
+            controller.stop_span_for_trace(None, None)
+
+        self.assertEqual(controller.retry_count, 0)
+
+
+@all_requests
+def response_success(url, request):
+    return {'status_code': 200}
+
+@all_requests
+def response_retry_limit(url, request):
+    return {'status_code': 429}
+
+@all_requests
+def response_retry_limit_with_after(url, request):
+    return {
+        'status_code': 429,
+        'headers': {'Retry-After': 1}
+    }
+
+@all_requests
+def response_retry_unavailable(url, request):
+    return {'status_code': 503}
+
+@all_requests
+def response_retry_unavailable_with_after(url, request):
+    return {
+        'status_code': 503,
+        'headers': {'Retry-After': 1}
+    }
