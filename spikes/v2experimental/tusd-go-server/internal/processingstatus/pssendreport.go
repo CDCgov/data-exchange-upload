6b159bd87e1d444230c5117a0e4caece5105fbb0
Cosmin Potocean <cosmycx@gmail.com>
2024-03-27 20:10:07 -0400

    v2 go ps sender client and sender msg struct json

--- spikes/v2experimental/tusd-go-server/internal/processingstatus/pssendreport.go
+++ spikes/v2experimental/tusd-go-server/internal/processingstatus/pssendreport.go
@@ -2,11 +2,13 @@ package processingstatus
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus"
+	"github.com/cdcgov/data-exchange-upload/tusd-go-server/internal/models"
 )
 
-func (pss PsSender) SendReport(message string) error { // TODO: probably not return if an error
+func (pss PsSender) SendReport(report models.PsReport) error { // TODO: probably not return if an error
 
 	// TODO: what happens when we can't send to processing status and error?
 	// should it even be returned? probably not so this can be called on a goroutine
@@ -17,10 +19,14 @@ func (pss PsSender) SendReport(message string) error { // TODO: probably not ret
 	} // .if
 	defer sender.Close(context.TODO())
 
+	jsonData, err := json.Marshal(report)
+	if err != nil {
+		return err
+	} // .if
+
 	sbMessage := &azservicebus.Message{
 
-		Body: []byte(message), // TODO change to .json report
-
+		Body: []byte(jsonData),
 	} // .sbMessage
 	err = sender.SendMessage(context.TODO(), sbMessage, nil)
 	if err != nil {
