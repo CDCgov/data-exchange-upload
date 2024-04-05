fdcb4bf0a4e838646829bf3fab2500e08cf91411
Hiwot Gebre <130766890+USZ7@users.noreply.github.com>
2024-04-05 16:13:07 -0400

    upload-1114 implement feature flagging (#280)
    * upload-1114 implement feature flagging
    
    * upload-1114 bug fix
    
    * upload-1114 addressed PR comments
    
    * upload-1114 additional PR comment fix
    
    * upload-1114 clean up
    
    ---------
    
    Co-authored-by: Gebre <USZ7-SU@cdc.gov>

--- tus/file-hooks/post-create/test/test_post_create.py
+++ tus/file-hooks/post-create/test/test_post_create.py
@@ -48,12 +48,19 @@ class TestPostCreateMethods(unittest.TestCase):
 
         self.assertEqual(result, 'example.txt')
   
+    @patch('post_create_bin.processing_status_traces_enabled', True)
     @patch.object(ProcStatController, 'create_upload_trace', return_value=("trace_id", "parent_span_id"))
     @patch.object(ProcStatController, 'start_span_for_trace', return_value=("trace_id", "metadata_verify_span_id"))
     @patch.object(ProcStatController, 'create_report_json')
     @patch.object(ProcStatController, 'stop_span_for_trace')
-    def test_should_send_create_trace_request(self, create_upload_trace_mock, start_span_for_trace_mock, create_report_json_mock, stop_span_for_trace_mock):
-        
+    def test_should_send_create_trace_request(
+        self,
+        stop_span_for_trace_mock,
+        create_report_json_mock,
+        start_span_for_trace_mock,
+        create_upload_trace_mock
+        ):
+        from post_create_bin import post_create
         use_case = 'use_case'
         use_case_category = 'some_use_case_category'
         metadata_json_dict = {'meta_destination_id': 'destination', 'meta_ext_event': 'some_event'}
@@ -62,7 +69,7 @@ class TestPostCreateMethods(unittest.TestCase):
         post_create(use_case, use_case_category, metadata_json_dict, tguid)
 
         self.assertEqual(create_upload_trace_mock.call_count, 1)
-        self.assertEqual(start_span_for_trace_mock.call_count, 1)
+        self.assertEqual(start_span_for_trace_mock.call_count, 2)
         self.assertEqual(stop_span_for_trace_mock.call_count, 1)
 
 if __name__ == "__main__":
