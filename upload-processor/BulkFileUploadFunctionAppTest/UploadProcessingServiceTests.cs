de42e26dbaf621c74fb4861519908c64d3187a3a
tedtally <xrq6@cdc.gov>
2024-04-23 12:12:49 -0400

    Refactor UploadProcessingServiceTests.cs and remove unused code

--- upload-processor/BulkFileUploadFunctionAppTest/UploadProcessingServiceTests.cs
+++ upload-processor/BulkFileUploadFunctionAppTest/UploadProcessingServiceTests.cs
@@ -21,77 +21,10 @@ namespace BulkFileUploadFunctionAppTests
     [TestClass]
     public class UploadProcessingServiceTests
     {
-        private UploadProcessingService? _uploadProcessingService;
-        private Mock<IProcStatClient>? _mockProcStatClient;
-        private Mock<IConfigurationManager>? _mockConfigManager;
-        private Mock<IFeatureManagementExecutor>? _mockFeatureManagementExecutor;
-        private Mock<IUploadEventHubService>? _mockUploadEventHubService;
-        private Mock<ILogger<UploadProcessingService>>? _loggerMock;
-        private Mock<ILoggerFactory>? _loggerFactoryMock;
-        private StorageBlobCreatedEvent? _storageBlobCreatedEvent;
-        private Mock<IUploadProcessingService>? _mockUploadProcessingService;
-        private Mock<AzureBlobReader>? _mockBlobReader;
-        private Mock<BlobClient>? _mockBlobClient;
-        private Mock<IBlobServiceClientFactory>? _mockBlobServiceClientFactory;
-        private Mock<BlobServiceClient>? _mockBlobServiceClient;
-        private Mock<BlobServiceClient>? _mockEdavBlobServiceClient;
-        private Mock<Uri>? _mockUri;
-        private Mock<ILogger<BlobServiceClientFactory>> _mockLogger;
-        private Mock<BlobContainerClient>? _mockBlobContainerClient;
-
         [TestInitialize]
         public void Initialize()
         {
-            _mockLogger = new Mock<ILogger<BlobServiceClientFactory>>();
-            _loggerFactoryMock = new Mock<ILoggerFactory>();
-            _loggerMock = new Mock<ILogger<UploadProcessingService>>();
-            _loggerFactoryMock.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(_loggerMock.Object);
-            _mockBlobClient = new Mock<BlobClient>();
-            _mockBlobServiceClient = new Mock<BlobServiceClient>();
-            _mockBlobServiceClientFactory = new Mock<IBlobServiceClientFactory>();
-            _mockBlobServiceClientFactory
-                .Setup(x=> x.CreateInstance(It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(_mockBlobServiceClient.Object);
-            _mockUri = new Mock<Uri>("https://example.com/blob/1MB-test-file.txt"); //new Mock<Uri>();
-            //_mockUri.Setup(x => x.AbsoluteUri).Returns("https://example.com/blob/1MB-test-file.txt");
-
-            _mockEdavBlobServiceClient = new Mock<BlobServiceClient>();
-            _mockBlobServiceClientFactory
-                .Setup(x => x.CreateInstance(It.IsAny<string>(), _mockUri.Object, It.IsAny<DefaultAzureCredential>()))
-                .Returns(_mockEdavBlobServiceClient.Object);
-
-            _mockBlobContainerClient = new Mock<BlobContainerClient>();
-            _mockConfigManager = new Mock<IConfigurationManager>();
-            _mockProcStatClient = new Mock<IProcStatClient>();
-            _mockFeatureManagementExecutor = new Mock<IFeatureManagementExecutor>();
-            _mockBlobReader = new Mock<AzureBlobReader>();
-            _mockUploadEventHubService = new Mock<IUploadEventHubService>();
-
-      
-
-
-            _storageBlobCreatedEvent = new StorageBlobCreatedEvent
-            {
-                Id = "12323",
-                Topic = "routineImmunization",
-                Subject = "IZGW",
-                EventType = "DD2",
-                EventTime = System.DateTime.Now,
-                Data = new StorageBlobCreatedEventData { Url = "https://example.com/blob/10MB-test-file" }
-            };
-
-
-
-            _mockUploadProcessingService = new Mock<IUploadProcessingService>();
-
-            _mockUploadProcessingService.CallBase = true;
-            //_uploadProcessingService = new UploadProcessingService(
-            //    _loggerFactoryMock.Object, 
-            //    _mockConfigManager.Object, 
-            //    _mockProcStatClient.Object, _mockFeatureManagementExecutor.Object,
-            //    _mockUploadEventHubService.Object, _mockBlobServiceClientFactory.Object);
-
-
+            //TODO: Add any initialization code here
         }
 
 
