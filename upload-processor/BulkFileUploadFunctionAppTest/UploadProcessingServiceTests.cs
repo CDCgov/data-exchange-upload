93cac7ee555a3422a50ada6090856649c7863272
tma5 <tma5@cdc.gov>
2024-04-24 13:59:50 -0400

    revert changes to unit test

--- upload-processor/BulkFileUploadFunctionAppTest/UploadProcessingServiceTests.cs
+++ upload-processor/BulkFileUploadFunctionAppTest/UploadProcessingServiceTests.cs
@@ -1,11 +1,62 @@
+using Azure.Storage.Blobs;
+using BulkFileUploadFunctionApp;
 using BulkFileUploadFunctionApp.Model;
 using BulkFileUploadFunctionApp.Services;
 using BulkFileUploadFunctionApp.Utils;
+using BulkFileUploadFunctionAppTest.utils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
 namespace BulkFileUploadFunctionAppTests
 {
     [TestClass]
     public class UploadProcessingServiceTests
     {
-        
+        private Mock<IProcStatClient>? _mockProcStatClient;
+        private Mock<ILogger<UploadProcessingService>>? _loggerMock;
+        private Mock<ILoggerFactory>? _loggerFactoryMock;
+        private StorageBlobCreatedEvent _storageBlobCreatedEvent;
+        private Mock<IUploadProcessingService>? _mockUploadProcessingService;
+        private Mock<IFeatureManagementExecutor>? _mockFeatureManagementExecutor;
+        private Mock<IBlobReader> _mockBlobReader;
+        private BlobReaderFactory _blobReaderFactory;
+        private Mock<BlobReaderFactory>? _mockBlobReaderFactory;
+        private string sourceContainerName;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            sourceContainerName = "bulkuploads";
+            _loggerFactoryMock = new Mock<ILoggerFactory>();
+            _loggerMock = new Mock<ILogger<UploadProcessingService>>();
+            _loggerFactoryMock.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(_loggerMock.Object);
+
+            _mockProcStatClient = new Mock<IProcStatClient>();
+            _mockFeatureManagementExecutor = new Mock<IFeatureManagementExecutor>();
+            _blobReaderFactory = new BlobReaderFactory();
+            _mockBlobReader = new Mock<IBlobReader>();
+            _mockBlobReaderFactory = new Mock<BlobReaderFactory>();
+            _mockBlobReaderFactory
+                .Setup(x => x.CreateInstance(It.IsAny<ILogger>()))
+                .Returns(_mockBlobReader.Object);
+
+            _storageBlobCreatedEvent = new StorageBlobCreatedEvent
+            {
+                Id = "12323",
+                Topic = "routineImmunization",
+                Subject = "IZGW",
+                EventType = "DD2",
+                EventTime = System.DateTime.Now,
+                Data = new StorageBlobCreatedEventData { Url = "https://example.com/blob/10MB-test-file" }
+            };
+
+            _mockBlobReaderFactory.Setup(x => x.CreateInstance(It.IsAny<ILogger>())).Returns(_mockBlobReader.Object);
+
+            _mockUploadProcessingService = new Mock<IUploadProcessingService>();
+
+            _mockUploadProcessingService.CallBase = true;
+
+        }
     }
 }
