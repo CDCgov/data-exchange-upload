20cdf5cbd131f0f950b839402a508486a5ddd25c
GSA2024 <165842677+GSA2024@users.noreply.github.com>
2024-05-16 14:34:07 -0400

    calean-up

--- tests/smoke/kotlin/src/test/kotlin/auth/AuthResponse.kt
+++
@@ -1,9 +0,0 @@
-package auth
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-data class AuthResponse(
-    @get:JsonProperty("access_token") val accessToken: String
-)
