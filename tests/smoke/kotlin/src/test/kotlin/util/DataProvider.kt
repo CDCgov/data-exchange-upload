f4d8da822c3442ec4857d10903003e35e74bafe3
tma5 <tma5@cdc.gov>
2024-05-31 14:14:30 -0400

    refactor all tests for file copy

--- tests/smoke/kotlin/src/test/kotlin/util/DataProvider.kt
+++ tests/smoke/kotlin/src/test/kotlin/util/DataProvider.kt
@@ -25,17 +25,33 @@ class DataProvider {
             validManifests.forEach {
                 val jsonBytes = TestFile.getResourceFile(it).readBytes()
                 val manifestJsons: List<HashMap<String, String>> = ObjectMapper().readValue(jsonBytes)
-                val filtered: List<HashMap<String, String>> = if (useCases.isNotEmpty()) manifestJsons.filter { m ->
-                    val useCase = if (m.containsKey("version") && m["version"] == "2.0")
-                        "${m["data_stream_id"]}-${m["data_stream_route"]}"
-                    else "${m["meta_destination_id"]}-${m["meta_ext_event"]}"
-
-                    useCases.contains(useCase)
-                } else manifestJsons
+                val filtered = filterByUseCases(useCases, manifestJsons)
                 manifests.addAll(filtered)
             }
 
             return manifests.map { arrayOf(it) }.toTypedArray()
+        }
+
+        @DataProvider(name = "validManifestV1Provider")
+        @JvmStatic
+        fun validManifestV1Provider(): Array<Array<HashMap<String, String>>> {
+            val useCases: List<String> = System.getProperty("useCases")?.split(",") ?: arrayListOf()
+
+            val jsonBytes = TestFile.getResourceFile("valid_manifests_v1.json").readBytes()
+            val manifestJsons: List<HashMap<String, String>> = ObjectMapper().readValue(jsonBytes)
+            val manifests = filterByUseCases(useCases, manifestJsons)
+
+            return manifests.map { arrayOf(it) }.toTypedArray()
+        }
+
+        private fun filterByUseCases(useCases: List<String>, manifests: List<HashMap<String, String>>): List<HashMap<String, String>> {
+            return if (useCases.isNotEmpty()) manifests.filter { m ->
+                val useCase = if (m.containsKey("version") && m["version"] == "2.0")
+                    "${m["data_stream_id"]}-${m["data_stream_route"]}"
+                else "${m["meta_destination_id"]}-${m["meta_ext_event"]}"
+
+                useCases.contains(useCase)
+            } else manifests
         }
     }
 }
