023881b8df8ebf9fed5b6acc2450e8ff2b59bd07
tma5 <tma5@cdc.gov>
2024-06-06 15:53:20 -0400

    refactor proc stat tests to use data provider

--- tests/smoke/kotlin/src/test/kotlin/ProcStat.kt
+++ tests/smoke/kotlin/src/test/kotlin/ProcStat.kt
@@ -1,4 +1,3 @@
-import org.testng.annotations.Test
 import tus.UploadClient
 import io.restassured.RestAssured.*
 import io.restassured.response.ValidatableResponse
@@ -9,11 +8,9 @@ import org.hamcrest.Matchers.*
 import org.testng.Assert.assertNotNull
 import org.testng.ITestContext
 import org.testng.TestNGException
-import org.testng.annotations.BeforeTest
-import org.testng.annotations.Listeners
-import org.testng.annotations.Optional
-import org.testng.annotations.Parameters
+import org.testng.annotations.*
 import util.*
+import util.DataProvider
 import kotlin.test.assertNull
 
 @Listeners(UploadIdTestListener::class)
@@ -25,85 +22,70 @@ class ProcStat {
     private val procStatReqSpec = given().apply {
         baseUri(EnvConfig.PROC_STAT_URL)
     }
+    private lateinit var authToken: String
+    private lateinit var testContext: ITestContext
+
     private lateinit var uploadClient: UploadClient
-    private lateinit var uploadId: String
-    private lateinit var reportResponse: ValidatableResponse
-    private lateinit var uploadConfig: UploadConfig
 
-    @Parameters("SENDER_MANIFEST", "USE_CASE")
     @BeforeTest(groups = [Constants.Groups.PROC_STAT])
-    fun beforeTest(
+    fun beforeProcStat(
         context: ITestContext,
-        @Optional SENDER_MANIFEST: String?,
-        @Optional("dextesting-testevent1") USE_CASE: String
     ) {
-        val authToken = authClient.getToken(EnvConfig.SAMS_USERNAME, EnvConfig.SAMS_PASSWORD)
+        authToken = authClient.getToken(EnvConfig.SAMS_USERNAME, EnvConfig.SAMS_PASSWORD)
+    }
+
+    @BeforeMethod
+    fun setupUpload(context: ITestContext) {
+        testContext = context
         uploadClient = UploadClient(EnvConfig.UPLOAD_URL, authToken)
-
-        val senderManifestDataFile = if (SENDER_MANIFEST.isNullOrEmpty()) "$USE_CASE.properties" else SENDER_MANIFEST
-        val propertiesFilePath = "properties/v1/$USE_CASE/$senderManifestDataFile"
-        val senderManifest = Metadata.convertPropertiesToMetadataMap(propertiesFilePath)
-
-        uploadConfig = ConfigLoader.loadUploadConfig(dexBlobClient, "$USE_CASE.json", "v1")
-
-        uploadId = uploadClient.uploadFile(testFile, senderManifest)
-                ?: throw TestNGException("Error uploading file ${testFile.name}")
-        context.setAttribute("uploadId", uploadId)
-
-        Thread.sleep(1_000) // Hard delay to wait for PS API to settle.
-
-        reportResponse = procStatReqSpec.get("/api/report/uploadId/$uploadId")
-                .then()
-                .statusCode(200)
     }
-    @Test(groups = [Constants.Groups.PROC_STAT, Constants.Groups.PROC_STAT_REPORT])
-    fun shouldHaveMetadataVerifyReportWhenFileUploaded() {
+
+    @Test(groups = [Constants.Groups.PROC_STAT], dataProvider = "validManifestAllProvider", dataProviderClass = DataProvider::class)
+    fun shouldHaveReportsForSuccessfulFileUpload(manifest: HashMap<String, String>) {
+        val config = ConfigLoader.loadUploadConfig(dexBlobClient, manifest)
+        val uid = uploadClient.uploadFile(testFile, manifest) ?: throw TestNGException("Error uploading file ${testFile.name}")
+        testContext.setAttribute("uploadId", uid)
+        Thread.sleep(500)
+
+        val reportResponse = procStatReqSpec.get("/api/report/uploadId/$uid")
+            .then()
+            .statusCode(200)
+
+        // Metadata Verify
         reportResponse.
-        body("upload_id", equalTo(uploadId))
+            body("upload_id", equalTo(uid))
                 .body("reports.stage_name",
-                hasItem("dex-metadata-verify")).body("reports.content.schema_name",
-                hasItem("dex-metadata-verify"))
-    }
+                    hasItem("dex-metadata-verify")).body("reports.content.schema_name",
+                    hasItem("dex-metadata-verify"))
 
-    @Test(groups = [Constants.Groups.PROC_STAT, Constants.Groups.PROC_STAT_REPORT])
-    fun shouldHaveNullIssuesArrayWhenFileUploaded() {
-        val jsonPath = reportResponse.extract().jsonPath()
+        var jsonPath = reportResponse.extract().jsonPath()
         val metadataVerifyReport = jsonPath.getList("reports", Report::class.java).find { it.stageName == "dex-metadata-verify" }
-
         assertNotNull(metadataVerifyReport)
         assertNull(metadataVerifyReport?.issues)
-    }
 
-    @Test(groups = [Constants.Groups.PROC_STAT, Constants.Groups.PROC_STAT_REPORT])
-    fun shouldHaveUploadStatusReportWhenFileUploaded() {
-        reportResponse.body("upload_id", equalTo(uploadId)).body("reports.stage_name", hasItem(Constants.UPLOAD_STATUS_REPORT_STAGE_NAME)).body("reports.content.schema_name", hasItem("upload"))
-    }
+        // Upload
+        reportResponse
+            .body("upload_id", equalTo(uid))
+                .body("reports.stage_name", hasItem(Constants.UPLOAD_STATUS_REPORT_STAGE_NAME))
+                    .body("reports.content.schema_name", hasItem("upload"))
 
-    @Test(groups = [Constants.Groups.PROC_STAT, Constants.Groups.PROC_STAT_REPORT])
-    fun shouldHaveEqualOffsetAndSizeWhenFileUploaded() {
-        val jsonPath = reportResponse.extract().jsonPath()
-        val uploadReport = jsonPath.getList("reports", Report::class.java).find { it.stageName == Constants.UPLOAD_STATUS_REPORT_STAGE_NAME }
-
-        assertNotNull(uploadReport)
-    }
-
-    @Parameters("EXPECTED_SOURCE_URL_PREFIXES", "EXPECTED_DESTINATION_URL_PREFIXES")
-    @Test(groups = [Constants.Groups.PROC_STAT, Constants.Groups.PROC_STAT_REPORT])
-    fun shouldHaveValidDestinationAndSourceURLWhenFileUploaded() {
-        // Parse the expected URLs from the parameters
-        val expectedDestinations = uploadConfig.copyConfig.targets
-
-        val jsonPath = reportResponse.extract().jsonPath()
+        jsonPath = reportResponse.extract().jsonPath()
         val reports = jsonPath.getList("reports", Report::class.java)
-        val dexFileCopyReports = reports.filterNotNull().filter { it.stageName == "dex-file-copy" }
+        val uploadReport = reports
+            .find { it.stageName == Constants.UPLOAD_STATUS_REPORT_STAGE_NAME }
+        assertNotNull(uploadReport)
 
+        val expectedDestinations = config.copyConfig.targets
+        val dexFileCopyReports = reports.filterNotNull().filter { it.stageName == "dex-file-copy" }
         assert(dexFileCopyReports.isNotEmpty()) { "No 'dex-file-copy' reports found" }
 
         val destinations = dexFileCopyReports.mapNotNull { it.content.destination }
 
         // Validate source URLs
         destinations.forEach { dest ->
-            assert(expectedDestinations.any { dest.trim().contains(it.trim()) }) { "Destination ${dest} does not match any expected destinations" }
+            assert(expectedDestinations.any { dest.trim().contains(it.trim()) }) { "Destination $dest does not match any expected destinations" }
         }
+
+        // TODO Post Processing
     }
 }
