d5a2f1b753e0f4e3863ed1f15df46d253837ad2f
tma5 <tma5@cdc.gov>
2024-06-06 11:54:39 -0400

    update clean script

--- upload-server/clean.sh
+++ upload-server/clean.sh
@@ -2,3 +2,6 @@ rm -rf uploads/reports
 rm -rf uploads/tus-prefix
 rm -rf testing/test/reports
 rm -rf testing/test/uploads
+rm -rf testing/test/dex
+rm -rf testing/test/edav
+rm -rf testing/test/routing
