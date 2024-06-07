4d5121bf132601a76f0d622a34a36f2cc6379c6c
Dave Tisza <dtisza@fearless.tech>
2024-06-07 10:04:32 -0400

    Add HEALTH_ISSUE_NONE msg file store health() UP

--- upload-server/cmd/cli/datastore.go
+++ upload-server/cmd/cli/datastore.go
@@ -85,5 +85,6 @@ func (c *FileStoreHealthCheck) Health(ctx context.Context) (rsp models.ServiceHe
 		return rsp
 	}
 	rsp.Status = models.STATUS_UP
+	rsp.HealthIssue = models.HEALTH_ISSUE_NONE
 	return rsp
 }
