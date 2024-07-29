b473b88c3828f8aed713a40f8b9992c55a759d0d
tma5 <tma5@cdc.gov>
2024-07-29 11:09:59 -0400

    more cleanup

--- upload-server/cmd/cli/reporters.go
+++ upload-server/cmd/cli/reporters.go
@@ -9,36 +9,11 @@ import (
 )
 
 func InitReporters(ctx context.Context, appConfig appconfig.AppConfig) error {
-	//reports.DefaultReporter = &filereporters.FileReporter{
-	//	Dir: appConfig.LocalReportsFolder,
-	//}
 	reports.DefaultReporter = &event.MemoryPublisher[reports.Report]{
 		Dir: appConfig.LocalReportsFolder,
 	}
 
 	if appConfig.ReporterConnection != nil && appConfig.ReporterConnection.ConnectionString != "" {
-		//sbClient, err := event.NewAMQPServiceBusClient(appConfig.ReporterConnection.ConnectionString)
-		//if err != nil {
-		//	return err
-		//}
-		//sender, err := sbClient.NewSender(appConfig.ReporterConnection.Queue, nil)
-		//if err != nil {
-		//	logger.Error("failed to configure report publisher", "error", err)
-		//	return err
-		//}
-		//adminClient, err := admin.NewClientFromConnectionString(appConfig.ReporterConnection.ConnectionString, nil)
-		//if err != nil {
-		//	logger.Error("failed to connect to service bus admin client", "error", err)
-		//	return err
-		//}
-		//
-		//r := &azurereporters.ServiceBusReporter{
-		//	Context:     ctx,
-		//	Sender:      sender,
-		//	AdminClient: adminClient,
-		//	QueueName:   appConfig.ReporterConnection.Queue,
-		//}
-
 		r, err := event.NewAzurePublisher[reports.Report](ctx, *appConfig.ReporterConnection, "Report")
 		if err != nil {
 			return err
