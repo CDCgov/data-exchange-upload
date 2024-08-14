f9e1fd4d99e976db834fb5b858f02a60932c56e9
tma5 <tma5@cdc.gov>
2024-08-14 19:25:25 -0400

    impl for s3

---
+++ upload-server/internal/loaders/s3.go
@@ -0,0 +1,25 @@
+package loaders
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"io"
+)
+
+type S3ConfigLoader struct {
+	Client     *s3.Client
+	BucketName string
+}
+
+func (l *S3ConfigLoader) LoadConfig(ctx context.Context, path string) ([]byte, error) {
+	output, err := l.Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: &l.BucketName,
+		Key:    &path,
+	})
+	if err != nil {
+
+		return nil, err
+	}
+
+	return io.ReadAll(output.Body)
+}
