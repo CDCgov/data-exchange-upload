36f42ccf79b280ec532482e07e04e4495c8c0dff
David Gage <whytheplatypus@gmail.com>
2024-08-16 11:49:35 -0400

    Working session round 1
    - forms and ui

--- upload-server/internal/ui/ui.go
+++ upload-server/internal/ui/ui.go
@@ -3,22 +3,92 @@ package ui
 import (
 	"context"
 	"embed"
+	"fmt"
 	"net/http"
+	"strings"
+
+	"html/template"
+
+	"github.com/cdcgov/data-exchange-upload/upload-server/internal/metadata"
+	"github.com/eventials/go-tus"
 )
 
 // content holds our static web server content.
 //
-//go:embed assets/* index.html
+//go:embed assets/* index.html destination/* manifest.tmpl
 var content embed.FS
 
-var Handler = http.FileServer(http.FS(content))
+func FixNames(name string) string {
+	removeChars := strings.ReplaceAll(name, "_", " ")
+	newName := strings.Title(strings.ToLower(removeChars))
+	return newName
+}
+
+var usefulFuncs = template.FuncMap{
+	"FixNames": FixNames,
+}
+
+var manifestTemplate = template.Must(template.New("manifest.tmpl").Funcs(usefulFuncs).ParseFS(content, "manifest.tmpl"))
+
+var StaticHandler = http.FileServer(http.FS(content))
 
 var DefaultServer = NewServer(":8000")
 
 func NewServer(addr string) *http.Server {
+	router := http.NewServeMux()
+	router.HandleFunc("/manifest", func(rw http.ResponseWriter, r *http.Request) {
+		// TODO check to see if they don't exist
+		dataStream := r.FormValue("data_stream")
+		dataStreamRoute := r.FormValue("data_stream_route")
+
+		config, err := metadata.Cache.GetConfig(r.Context(), fmt.Sprintf("v2/%s-%s.json", dataStream, dataStreamRoute))
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusNotFound)
+			return
+		}
+
+		manifestTemplate.Execute(rw, config)
+	})
+	router.HandleFunc("/upload", func(rw http.ResponseWriter, r *http.Request) {
+		// Tell the tus server we want to start an upload
+		// turn form values into map[string]string
+		r.ParseForm()
+		manifest := map[string]string{}
+		for k, v := range r.Form {
+			manifest[k] = v[0]
+		}
+
+		// submit to upload server to get upload id
+		upload := &tus.Upload{
+			Metadata: manifest,
+		}
+
+		req, err := http.NewRequest("POST", "http://localhost:8080/files/", nil)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		req.Header.Set("Content-Length", "0")
+		req.Header.Set("Upload-Metadata", upload.EncodedMetadata())
+		req.Header.Set("Upload-Defer-Length", "1")
+		req.Header.Set("Tus-Resumable", "1.0.0")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf(resp.Header.Get("Location"))
+
+		// redirect to page for file upload based on "Location" response
+		http.Redirect(rw, r, resp.Header.Get("Location"), 302)
+	})
+	router.Handle("/", StaticHandler)
+
 	s := &http.Server{
 		Addr:    addr,
-		Handler: Handler,
+		Handler: router,
 	}
 	return s
 }
