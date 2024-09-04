b4d3af3251684bbd1792fc82a9e904a5b1044163
Nicole Zonnenberg <nzonnenberg@fearless.tech>
2024-09-04 08:43:43 -0400

    add skip to main content

--- upload-server/internal/ui/assets/banner.js
+++ upload-server/internal/ui/assets/banner.js
@@ -3,6 +3,7 @@
 
   if (banner) {
     banner.innerHTML = `
+      <a class="skip-main" href="#main" tabindex="0">Skip to main content</a>
       <div role="navigation" id="nav" class="nav-container">
         <a href="/">
           <img src="/assets/dex_logo.svg" alt="DEX logo" /> Upload
