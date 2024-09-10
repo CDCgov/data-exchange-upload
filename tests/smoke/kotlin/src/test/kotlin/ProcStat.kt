f000ed8db2acfb928bf2145656a5c3ee4d04bd4f
GSA2024 <165842677+GSA2024@users.noreply.github.com>
2024-09-10 15:11:32 -0400

    Removed try-catch block

--- tests/smoke/kotlin/src/test/kotlin/ProcStat.kt
+++ tests/smoke/kotlin/src/test/kotlin/ProcStat.kt
@@ -49,97 +49,97 @@ class ProcStat {
         testContext.setAttribute("uploadId", uid)
         Thread.sleep(2000)
 
-        try {
-
-            val response = procStatReqSpec
-                .body(
-                    """
+        val response = procStatReqSpec
+            .body(
+                """
                 {
                  "query": "query GetReports { getReports(uploadId: \"$uid\", reportsSortedBy: \"timestamp\", sortOrder: Ascending) { content contentType data dataProducerId dataStreamId dataStreamRoute dexIngestDateTime id jurisdiction reportId senderId tags timestamp uploadId stageInfo { action endProcessingTime service startProcessingTime status version issues { level message } } } }",
                   "variables": {}
                 }
                 """.trimIndent()
-                )
-                .header("Content-Type", "application/json")
-                .log().all()
-                .post("pstatus/graphql-service/graphql")
+            )
+            .header("Content-Type", "application/json")
+            .log().all()
+            .post("pstatus/graphql-service/graphql")
 
-            val jsonResponse = response.asString()
-            val objectMapper = jacksonObjectMapper()
-            val dataResponse: DataResponse = objectMapper.readValue(jsonResponse, DataResponse::class.java)
+        val jsonResponse = response.asString()
+        val objectMapper = jacksonObjectMapper()
+        val dataResponse: DataResponse = objectMapper.readValue(jsonResponse, DataResponse::class.java)
 
-            val reportList = dataResponse.data.reports
+        val reportList = dataResponse.data.reports
 
-            reportList.forEach { report ->
-                val schemaName = report.content.contentSchemaName
-                when (schemaName) {
+        reportList.forEach { report ->
+            val schemaName = report.content.contentSchemaName
+            when (schemaName) {
 
-                    "metadata-verify" -> {
-                        val uploadId = report.content.metadata?.uploadId
-                        val dexIngestDateTime = report.content.metadata?.dexIngestDateTime
-                        val stageInfo = report.stageInfo
+                "metadata-verify" -> {
+                    val uploadId = report.content.metadata?.uploadId
+                    val dexIngestDateTime = report.content.metadata?.dexIngestDateTime
+                    val stageInfo = report.stageInfo
 
-                        log.info("Metadata Verify - Upload ID: $uploadId, Dex Ingest DateTime: $dexIngestDateTime")
+                    log.info("Metadata Verify - Upload ID: $uploadId, Dex Ingest DateTime: $dexIngestDateTime")
 
-                        assertEquals(uid, uploadId, "Expected upload ID to match the UID, but found: $uploadId")
-                        assertEquals("SUCCESS", stageInfo?.status, "Expected status 'SUCCESS' for metadata verify, but found: ${stageInfo?.status}")
-                        assertTrue(stageInfo?.issues.isNullOrEmpty(), "Expected no issues in the metadata verify report, but found: ${stageInfo?.issues}")
-                    }
+                    assertEquals(uid, uploadId, "Expected upload ID to match the UID, but found: $uploadId")
+                    assertEquals(
+                        "SUCCESS",
+                        stageInfo?.status,
+                        "Expected status 'SUCCESS' for metadata verify, but found: ${stageInfo?.status}"
+                    )
+                    assertTrue(
+                        stageInfo?.issues.isNullOrEmpty(),
+                        "Expected no issues in the metadata verify report, but found: ${stageInfo?.issues}"
+                    )
+                }
 
 
-                    "metadata-transform" -> {
-                        report.content.transforms?.forEach { transform ->
-                            val action = transform.action
-                            val field = transform.field
-                            val value = transform.value
-                            log.info("Transform Action: $action, Field: $field, Value: $value")
-                        }
-                    }
-
-                    "upload-started", "upload-completed" -> {
-                        val status = report.content.status
-                        assertEquals("SUCCESS", status, "Expected status 'SUCCESS' for $schemaName, but found: $status")
-                        log.info("Schema Name: $schemaName, Status: $status")
-                    }
-
-                    "upload-status" -> {
-                        val filename = report.content.filename
-                        val tguid = report.content.tguid
-                        val offset = report.content.offset
-                        val size = report.content.size
-                        log.info("Upload Status - Filename: $filename, TGUID: $tguid, Offset: $offset, Size: $size")
-                    }
-
-                    "blob-file-copy" -> {
-                        val sourceUrl = report.content.fileSourceBlobUrl
-                        val destinationUrl = report.content.fileDestinationBlobUrl
-                        val destinationName = report.content.destinationName
-                        assertNotNull(sourceUrl, "Blob source URL is missing for $schemaName")
-                        assertNotNull(destinationUrl, "Blob destination URL is missing for $schemaName")
-                        assertNotNull(destinationName, "Destination name is missing for $schemaName")
-                        log.info("Blob File Copy - Source URL: $sourceUrl, Destination URL: $destinationUrl")
-
-                        val expectedDestinations = config.copyConfig.targets
-
-                        val actualDestination = expectedDestinations.any { expectedDest ->
-                            destinationName?.trim()?.equals(expectedDest.trim(), ignoreCase = true) ?: false
-                        }
-
-                        assertTrue(
-                            actualDestination,
-                            "None of the expected destinations ('${expectedDestinations.joinToString(", ")}') were found in the response. Found: $destinationName"
-                        )
-                    }
-
-                    else -> {
-                        log.info("Unknown schema name: $schemaName")
+                "metadata-transform" -> {
+                    report.content.transforms?.forEach { transform ->
+                        val action = transform.action
+                        val field = transform.field
+                        val value = transform.value
+                        log.info("Transform Action: $action, Field: $field, Value: $value")
                     }
                 }
-            }
 
-        } catch (e: Exception) {
-            log.error("Test failed due to exception: ${e.message}", e)
-            throw TestNGException("Test failed due to exception: ${e.message}")
+                "upload-started", "upload-completed" -> {
+                    val status = report.content.status
+                    assertEquals("SUCCESS", status, "Expected status 'SUCCESS' for $schemaName, but found: $status")
+                    log.info("Schema Name: $schemaName, Status: $status")
+                }
+
+                "upload-status" -> {
+                    val filename = report.content.filename
+                    val tguid = report.content.tguid
+                    val offset = report.content.offset
+                    val size = report.content.size
+                    log.info("Upload Status - Filename: $filename, TGUID: $tguid, Offset: $offset, Size: $size")
+                }
+
+                "blob-file-copy" -> {
+                    val sourceUrl = report.content.fileSourceBlobUrl
+                    val destinationUrl = report.content.fileDestinationBlobUrl
+                    val destinationName = report.content.destinationName
+                    assertNotNull(sourceUrl, "Blob source URL is missing for $schemaName")
+                    assertNotNull(destinationUrl, "Blob destination URL is missing for $schemaName")
+                    assertNotNull(destinationName, "Destination name is missing for $schemaName")
+                    log.info("Blob File Copy - Source URL: $sourceUrl, Destination URL: $destinationUrl")
+
+                    val expectedDestinations = config.copyConfig.targets
+
+                    val actualDestination = expectedDestinations.any { expectedDest ->
+                        destinationName?.trim()?.equals(expectedDest.trim(), ignoreCase = true) ?: false
+                    }
+
+                    assertTrue(
+                        actualDestination,
+                        "None of the expected destinations ('${expectedDestinations.joinToString(", ")}') were found in the response. Found: $destinationName"
+                    )
+                }
+
+                else -> {
+                    log.info("Unknown schema name: $schemaName")
+                }
+            }
         }
     }
 }
