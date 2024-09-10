82e9ea116eab23637e6e9a888892c27a6de74f58
GSA2024 <165842677+GSA2024@users.noreply.github.com>
2024-09-10 07:05:06 -0400

    Response validation for successful file upload and various schema types like metadata-verify, upload-status and blob-file-copy

--- tests/smoke/kotlin/src/test/kotlin/ProcStat.kt
+++ tests/smoke/kotlin/src/test/kotlin/ProcStat.kt
@@ -1,15 +1,15 @@
 import tus.UploadClient
 import io.restassured.RestAssured.*
 import dex.DexUploadClient
-import model.Report
-import org.hamcrest.Matchers.*
-import org.testng.Assert.assertNotNull
 import org.testng.ITestContext
 import org.testng.TestNGException
 import org.testng.annotations.*
 import util.*
 import util.DataProvider
-import kotlin.test.assertNull
+import org.testng.Assert.*
+import org.slf4j.LoggerFactory
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import model.DataResponse
 
 @Listeners(UploadIdTestListener::class)
 @Test()
@@ -19,17 +19,15 @@ class ProcStat {
     private val dexBlobClient = Azure.getBlobServiceClient(EnvConfig.DEX_STORAGE_CONNECTION_STRING)
     private val procStatReqSpec = given().relaxedHTTPSValidation()
         .apply {
-        baseUri(EnvConfig.PROC_STAT_URL)
-    }
+            baseUri(EnvConfig.PROC_STAT_URL)
+        }
     private lateinit var authToken: String
     private lateinit var testContext: ITestContext
-
     private lateinit var uploadClient: UploadClient
+    private val log = LoggerFactory.getLogger(ProcStat::class.java)
 
     @BeforeTest(groups = [Constants.Groups.PROC_STAT])
-    fun beforeProcStat(
-        context: ITestContext,
-    ) {
+    fun beforeProcStat(context: ITestContext) {
         authToken = authClient.getToken(EnvConfig.SAMS_USERNAME, EnvConfig.SAMS_PASSWORD)
     }
 
@@ -44,65 +42,96 @@ class ProcStat {
         dataProvider = "validManifestAllProvider",
         dataProviderClass = DataProvider::class
     )
-    fun shouldHaveReportsForSuccessfulFileUpload(manifest: HashMap<String, String>) {
+    fun shouldHaveReportsForSuccessfulFileUpload(manifest: HashMap<String, String>, testContext: ITestContext) {
         val config = ConfigLoader.loadUploadConfig(dexBlobClient, manifest)
         val uid = uploadClient.uploadFile(testFile, manifest)
             ?: throw TestNGException("Error uploading file ${testFile.name}")
         testContext.setAttribute("uploadId", uid)
         Thread.sleep(2000)
 
-        val reportResponse = procStatReqSpec
-            .body( """
-            {
-                "query": "query GetReports { getReports(uploadId: \"$uid\", reportsSortedBy: \"timestamp\", sortOrder: Ascending) { content contentType data dataStreamId dataStreamRoute dexIngestDateTime id jurisdiction reportId senderId tags timestamp uploadId } }",
-                "variables": {}
+        try {
+
+            val response = procStatReqSpec
+                .body(
+                    """
+                {
+                    "query": "query GetReports { getReports(uploadId: \"$uid\", reportsSortedBy: \"timestamp\", sortOrder: Ascending) { content contentType data dataStreamId dataStreamRoute dexIngestDateTime id jurisdiction reportId senderId tags timestamp uploadId } }",
+                    "variables": {}
+                }
+                """.trimIndent()
+                )
+                .header("Content-Type", "application/json")
+                .log().all()
+                .post("pstatus/graphql-service/graphql")
+
+            val jsonResponse = response.asString()
+            val objectMapper = jacksonObjectMapper()
+            val dataResponse: DataResponse = objectMapper.readValue(jsonResponse, DataResponse::class.java)
+
+            val reportList = dataResponse.data.reports
+
+            reportList.forEach { report ->
+                val schemaName = report.content.contentSchemaName
+                when (schemaName) {
+                    "metadata-verify" -> {
+                        val uploadId = report.content.metadata?.uploadId
+                        val dexIngestDateTime = report.content.metadata?.dexIngestDateTime
+                        log.info("Metadata Verify - Upload ID: $uploadId, Dex Ingest DateTime: $dexIngestDateTime")
+                    }
+
+                    "metadata-transform" -> {
+                        report.content.transforms?.forEach { transform ->
+                            val action = transform.action
+                            val field = transform.field
+                            val value = transform.value
+                            log.info("Transform Action: $action, Field: $field, Value: $value")
+                        }
+                    }
+
+                    "upload-started", "upload-completed" -> {
+                        val status = report.content.status
+                        assertEquals("SUCCESS", status, "Expected status 'SUCCESS' for $schemaName, but found: $status")
+                        log.info("Schema Name: $schemaName, Status: $status")
+                    }
+
+                    "upload-status" -> {
+                        val filename = report.content.filename
+                        val tguid = report.content.tguid
+                        val offset = report.content.offset
+                        val size = report.content.size
+                        log.info("Upload Status - Filename: $filename, TGUID: $tguid, Offset: $offset, Size: $size")
+                    }
+
+                    "blob-file-copy" -> {
+                        val sourceUrl = report.content.fileSourceBlobUrl
+                        val destinationUrl = report.content.fileDestinationBlobUrl
+                        val destinationName = report.content.destinationName
+                        assertNotNull(sourceUrl, "Blob source URL is missing for $schemaName")
+                        assertNotNull(destinationUrl, "Blob destination URL is missing for $schemaName")
+                        assertNotNull(destinationName, "Destination name is missing for $schemaName")
+                        log.info("Blob File Copy - Source URL: $sourceUrl, Destination URL: $destinationUrl")
+
+                        val expectedDestinations = config.copyConfig.targets
+
+                        val actualDestination = expectedDestinations.any { expectedDest ->
+                            destinationName?.trim()?.equals(expectedDest.trim(), ignoreCase = true) ?: false
+                        }
+
+                        assertTrue(
+                            actualDestination,
+                            "None of the expected destinations ('${expectedDestinations.joinToString(", ")}') were found in the response. Found: $destinationName"
+                        )
+                    }
+
+                    else -> {
+                        log.info("Unknown schema name: $schemaName")
+                    }
+                }
             }
-        """.trimIndent())
-            .header("Content-Type", "application/json")
-            .post("pstatus/graphql-service/graphql")
-            .then()
-            .statusCode(200)
 
-
-        // Metadata Verify
-        reportResponse.body("upload_id", equalTo(uid))
-            .body(
-                "reports.stage_name",
-                hasItem("metadata-transform")
-            ).body(
-                "reports.content.schema_name",
-                hasItem("metadata-transform")
-            )
-
-        var jsonPath = reportResponse.extract().jsonPath()
-        val metadataVerifyReport =
-            jsonPath.getList("reports", Report::class.java).find { it.stageName == "metadata-transform" }
-        assertNotNull(metadataVerifyReport)
-        assertNull(metadataVerifyReport?.issues)
-
-        // Upload
-        reportResponse
-            .body("upload_id", equalTo(uid))
-            .body("reports.stage_name", hasItem(Constants.UPLOAD_STATUS_REPORT_STAGE_NAME))
-            .body("reports.content.schema_name", hasItem("upload"))
-
-        jsonPath = reportResponse.extract().jsonPath()
-        val reports = jsonPath.getList("reports", Report::class.java)
-        val uploadReport = reports
-            .find { it.stageName == Constants.UPLOAD_STATUS_REPORT_STAGE_NAME }
-        assertNotNull(uploadReport)
-
-        val expectedDestinations = config.copyConfig.targets
-        val dexFileCopyReports = reports.filterNotNull().filter { it.stageName == "dex-file-copy" }
-        assert(dexFileCopyReports.isNotEmpty()) { "No 'dex-file-copy' reports found" }
-
-        val destinations = dexFileCopyReports.mapNotNull { it.content.destination }
-
-        // Post Processing
-        expectedDestinations.forEach { dest ->
-            assert(destinations.any {
-                dest.trim().contains(it.trim())
-            }) { "Destination $dest was not found in copy reports" }
+        } catch (e: Exception) {
+            log.error("Test failed due to exception: ${e.message}", e)
+            throw TestNGException("Test failed due to exception: ${e.message}")
         }
     }
 }
