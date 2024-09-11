079a86f61edde6cd86350d0b96723c44f0866ef4
Arunajyothi Bhumireddy <165842677+GSA2024@users.noreply.github.com>
2024-09-11 11:03:37 -0400

    Automate graphql get reports functionalities (#474)
    * Added invalid_manifests_required_fields json file for negative testing
    
    * Added ndlp v2 manifests
    
    * Added manifests for negative testing ndlp useCases
    
    * Added ED3N manifests for Happy path and sad path scenarios
    
    * Invalid vales added for additional manifests
    
    * Modified method for helath check to add redis service and azure storage accounts in test
    
    * Added redis locker only for helath check method
    
    * PulseNet - Metadata Smoke Test Additions
    
    * Updated function ilterByUseCases additional criteria k/v pair for list of manifests based on specified use cases
    
    * Added a function filterByUseCases to filter by UseCases for any filed
    
    * Refined code to handle multiple use cases
    
    * created a common method that reads and filters the manifests based on the provided system properties this will eliminate duplicated code
    
    * Updated README doc which explains new command line arg works with examples
    
    * modified dataProvider utiliy class and added ReadMe doc for its usage with examples
    
    * Restore gradle-wrapper.jar from main branch
    
    * keeping gradle properties/files as-is
    
    * Renamed pairs to fields in DataProvider class
    
    * Updated 'loadAndFilterManifests' function to allow one delimiter ':' for key-value pairs
    
    * updated parseFilterValues function, to handle cases with no comma: return single value as list if values.size is 1, else throw an exception for more than two elements.
    
    * Added print statements filtered manifests against the size of the set of all manifests.
    
    * initial setup for graphql getReports Functionalities
    
    * Response validation for successful file upload and various schema types
    like metadata-verify, upload-status and blob-file-copy
    
    * Updated data models in Report.kt, ReportContent.kt, and MetadataConfig.kt to handle PS-API report data, content schema, transformations, and metadata.
    
    * Added assertions for metadat-verify report
    
    * Removed try-catch block
    
    * Added assertions for metadata-transform report to ensure presence of at least one transform
    
    * Added assertion for upload-status to ensure offset match
    
    * Added debug logs
    
    * Added additional debug logs

--- tests/smoke/kotlin/src/test/kotlin/model/MetadataConfig.kt
+++ tests/smoke/kotlin/src/test/kotlin/model/MetadataConfig.kt
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class MetadataConfig (
-
     @JsonProperty("version") val version: String,
     @JsonProperty("fields") val fields: List<FieldConfig>
 )
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Metadata(
+    @JsonProperty("upload_id") val uploadId: String?,
+    @JsonProperty("dex_ingest_datetime") val dexIngestDateTime: String?
+)
