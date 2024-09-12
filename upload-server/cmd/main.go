82a3f13c083e79e7290f0e0df9c66161b883a0c8
David Gage <whytheplatypus@gmail.com>
2024-09-12 10:52:43 -0400

    Starting to refactor highest level tests

--- upload-server/cmd/main.go
+++ upload-server/cmd/main.go
@@ -7,10 +7,8 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"runtime/debug"
 	"sync"
 	"syscall"
-	"testing"
 	"time"
 
 	"github.com/cdcgov/data-exchange-upload/upload-server/internal/event"
@@ -18,39 +16,17 @@ import (
 	"github.com/cdcgov/data-exchange-upload/upload-server/internal/postprocessing"
 	"github.com/cdcgov/data-exchange-upload/upload-server/internal/ui"
 	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/reports"
+	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/sloger"
+	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/slogerxexp"
 
 	"github.com/cdcgov/data-exchange-upload/upload-server/cmd/cli"
 	"github.com/cdcgov/data-exchange-upload/upload-server/internal/appconfig"
-	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/sloger"
-	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/slogerxexp"
 	"github.com/joho/godotenv"
 ) // .import
 
 const appMainExitCode = 1
 
-var (
-	appConfig appconfig.AppConfig
-	logger    *slog.Logger
-)
-
-// NOTE: this large init file may be an antipattern.
-// A main reason for it is to enable to cross cutting logging aspect.
-// If another way is found to manage that this should be moved to main.
 func init() {
-	ctx := context.Background()
-
-	buildInfo, _ := debug.ReadBuildInfo()
-	logInfo := []any{"buildInfo.Main.Path", buildInfo.Main.Path}
-	slog.With(logInfo...)
-	// ------------------------------------------------------------------
-	// parse and load cli flags
-	// ------------------------------------------------------------------
-	if !testing.Testing() {
-		if err := cli.ParseFlags(); err != nil {
-			slog.Error("error starting app, error parsing cli flags", "error", err)
-			os.Exit(appMainExitCode)
-		} // .if
-	}
 
 	if cli.Flags.AppConfigPath != "" {
 		slog.Info("Loading environment from", "file", cli.Flags.AppConfigPath)
@@ -63,51 +39,45 @@ func init() {
 	// ------------------------------------------------------------------
 	// parse and load config from os exported
 	// ------------------------------------------------------------------
-	var err error
-	appConfig, err = appconfig.ParseConfig(ctx)
-	if err != nil {
-		slog.Error("error starting app, error parsing app config", "error", err)
-		os.Exit(appMainExitCode)
-	} // .if
-
-	// ------------------------------------------------------------------
-	// configure app custom logging
-	// ------------------------------------------------------------------
-	logInfo = append(logInfo, "pkg", "main")
-	logger = cli.AppLogger(appConfig).With(logInfo...)
-	sloger.SetDefaultLogger(logger)
-
-	explogger := cli.ExpAppLogger(appConfig).With(logInfo...)
-	slogerxexp.SetDefaultLogger(explogger)
-
 }
 
 func main() {
 	ctx, cancelFunc := context.WithCancel(context.Background())
 	var mainWaitGroup sync.WaitGroup
 
-	logger.Info("starting app")
+	appConfig, err := appconfig.ParseConfig(ctx)
+	if err != nil {
+		slog.Error("error starting app, error parsing app config", "error", err)
+		os.Exit(appMainExitCode)
+	} // .if
+
+	sloger.SetDefaultLogger(cli.AppLogger(appConfig))
+	slog.SetDefault(sloger.DefaultLogger)
+	slogerxexp.SetDefaultLogger(cli.ExpAppLogger(appConfig))
+	slog.Info("starting app")
 
 	// Pub Sub
 	event.MaxRetries = appConfig.EventMaxRetryCount
 	// initialize event reporter
-	err := cli.InitReporters(ctx, appConfig)
+	if err := cli.InitReporters(ctx, appConfig); err != nil {
+		slog.Error("error creating reporters", "error", err)
+		os.Exit(appMainExitCode)
+	}
 	defer reports.CloseAll()
 
 	event.InitFileReadyChannel()
 	defer event.CloseFileReadyChannel()
 
-	err = event.InitFileReadyPublisher(ctx, appConfig)
-	defer event.FileReadyPublisher.Close()
-	if err != nil {
-		logger.Error("error creating file ready publisher", "error", err)
+	if err := event.InitFileReadyPublisher(ctx, appConfig); err != nil {
+		slog.Error("error creating file ready publisher", "error", err)
 		os.Exit(appMainExitCode)
 	}
+	defer event.FileReadyPublisher.Close()
 
 	mainWaitGroup.Add(1)
 	subscriber, err := cli.NewEventSubscriber[*event.FileReady](ctx, appConfig)
 	if err != nil {
-		logger.Error("error subscribing to file ready", "error", err)
+		slog.Error("error subscribing to file ready", "error", err)
 		os.Exit(appMainExitCode)
 	}
 	defer subscriber.Close()
@@ -119,11 +89,11 @@ func main() {
 	// start serving the app
 	handler, err := cli.Serve(ctx, appConfig)
 	if err != nil {
-		logger.Error("error starting app, error initialize dex handler", "error", err)
+		slog.Error("error starting app, error initialize dex handler", "error", err)
 		os.Exit(appMainExitCode)
 	}
 
-	logger.Info("http handlers ready")
+	slog.Info("http handlers ready")
 	// ------------------------------------------------------------------
 	// Start http custom server
 	// ------------------------------------------------------------------
@@ -140,24 +110,24 @@ func main() {
 	go func() {
 		err := httpServer.ListenAndServe()
 		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			logger.Error("error starting app, error starting http server", "error", err, "port", appConfig.ServerPort)
+			slog.Error("error starting app, error starting http server", "error", err, "port", appConfig.ServerPort)
 			os.Exit(appMainExitCode)
 		} // .if
 		mainWaitGroup.Done()
 	}() // .go
 
-	logger.Info("started http server with tusd and dex handlers", "port", appConfig.ServerPort)
+	slog.Info("started http server with tusd and dex handlers", "port", appConfig.ServerPort)
 
 	mainWaitGroup.Add(1)
 	go func() {
 		defer mainWaitGroup.Done()
 		if err := ui.Start(appConfig.UIPort, appConfig.TusUIFileEndpointUrl, appConfig.TusUIInfoEndpointUrl); err != nil {
-			logger.Error("failed to start ui", "error", err)
+			slog.Error("failed to start ui", "error", err)
 			os.Exit(appMainExitCode)
 		}
 	}()
 
-	logger.Info("Started ui server", "port", appConfig.UIPort)
+	slog.Info("Started ui server", "port", appConfig.UIPort)
 
 	// ------------------------------------------------------------------
 	// 	Block for Exit, server above is on goroutine
@@ -176,5 +146,5 @@ func main() {
 
 	mainWaitGroup.Wait()
 
-	logger.Info("closing server by os signal", "port", appConfig.ServerPort)
+	slog.Info("closing server by os signal", "port", appConfig.ServerPort)
 } // .main
--- upload-server/cmd/main_test.go
+++ upload-server/cmd/main_test.go
@@ -4,6 +4,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"os"
 	"sync"
 	"syscall"
@@ -14,13 +17,13 @@ import (
 )
 
 func TestTus(t *testing.T) {
-	url := "http://localhost:8080/files/"
+	url := fmt.Sprintf("http://localhost:%s", os.Getenv("SERVER_PORT"))
 	var wg sync.WaitGroup
 	for name, c := range dexTesting.Cases {
 		wg.Add(1)
 		go func(t *testing.T) {
 			defer wg.Done()
-			if err := dexTesting.RunTusTestCase(url, "../testing/test/test.txt", c); err != nil {
+			if _, err := dexTesting.RunTusTestCase(url, "../testing/test/test.txt", c); err != nil {
 				t.Error(name, err)
 			} else {
 				t.Log("test case", name, "passed")
@@ -30,14 +33,37 @@ func TestTus(t *testing.T) {
 	wg.Wait()
 }
 
-func TestMain(m *testing.M) {
-	//check integration test env var
-	if _, ok := os.LookupEnv("PODMAN_CI_TESTS"); ok {
-		go main()
-		// wait for main to start (should make this more resilient)
-		time.Sleep(5 * time.Second)
-		result := m.Run()
-		syscall.Kill(os.Getpid(), syscall.SIGINT)
-		os.Exit(result)
+// GetFreePort asks the kernel for a free open port that is ready to use.
+// credit: https://gist.github.com/sevkin/96bdae9274465b2d09191384f86ef39d
+func GetFreePort() (port int, err error) {
+	var a *net.TCPAddr
+	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
+		var l *net.TCPListener
+		if l, err = net.ListenTCP("tcp", a); err == nil {
+			defer l.Close()
+			return l.Addr().(*net.TCPAddr).Port, nil
+		}
 	}
+	return
+}
+
+func init() {
+	// clear the environment to prevent anything exciting
+	os.Clearenv()
+	port, err := GetFreePort()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Setenv("SERVER_PORT", fmt.Sprintf("%d", port))
+	os.Setenv("UPLOAD_CONFIG_PATH", "../../upload-configs")
+}
+
+func TestMain(m *testing.M) {
+
+	go main()
+	// wait for main to start (should make this more resilient)
+	time.Sleep(1 * time.Second)
+	result := m.Run()
+	syscall.Kill(os.Getpid(), syscall.SIGINT)
+	os.Exit(result)
 }
