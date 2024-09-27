e516cd1bc31f62f86de45271eb1a5289d414a7fe
GSA2024 <165842677+GSA2024@users.noreply.github.com>
2024-09-27 08:10:29 -0400

    Update tests for info delivery and upload-status validations

---
+++ tests/smoke/kotlin/src/test/kotlin/model/FileDelivery.kt
@@ -0,0 +1,11 @@
+package model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class FileDelivery(
+    val status: String,
+    val name: String,
+    val location: String,
+    @get:JsonProperty("delivered_at") val deliveredAt: String,
+    val issues: List<String>?
+)
