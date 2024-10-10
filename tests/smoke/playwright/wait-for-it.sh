7ca5f070cdd472662f3f621585fd9dd6665adce2
Tiffany Forkner <tforkner@fearless.tech>
2024-10-10 18:15:05 -0400

    removed restarts; using UI_URL in wait-for-it script

--- tests/smoke/playwright/wait-for-it.sh
+++ tests/smoke/playwright/wait-for-it.sh
@@ -5,22 +5,29 @@
 #
 # Wait until URL returns HTTP 200
 #
-# Example: ./wait_for_it.sh  "http://192.168.56.101:8080"
+# Example: ./wait_for_it.sh "http://192.168.56.101:8080"
 ##############################################################################################
 
 wait-for-url() {
-  timeout --foreground -s TERM 60s bash -c \
-      'while [[ "$(curl -s -o /dev/null -m 3 -L -w ''%{http_code}'' ${0})" != "200" ]];\
-      do sleep 2;\
-      done' ${1}
-  local TIMEOUT_RETURN="$?"
-  if [[ "${TIMEOUT_RETURN}" == 0 ]]; then
-      return 
-  elif [[ "${TIMEOUT_RETURN}" == 124 ]]; then
-      exit "${TIMEOUT_RETURN}"
-  else
-      exit "${TIMEOUT_RETURN}"
-  fi
+    echo "Testing: $1"
+    timeout --foreground -s TERM 60s bash -c \
+        'while [[ "$(curl -s -o /dev/null -m 3 -L -w ''%{http_code}'' ${0})" != "200" ]];\
+        do echo "Waiting for ${0}" && sleep 2;\
+        done' ${1}
+    local TIMEOUT_RETURN="$?"
+    if [[ "${TIMEOUT_RETURN}" == 0 ]]; then
+        echo "OK: ${1}"
+        return 
+    elif [[ "${TIMEOUT_RETURN}" == 124 ]]; then
+        echo "TIMEOUT: ${1} -> EXIT"
+        exit "${TIMEOUT_RETURN}"
+    else
+        echo "Other error with code ${TIMEOUT_RETURN}: ${1} -> EXIT"
+        exit "${TIMEOUT_RETURN}"
+    fi
 }
 
+echo "Wait for URL: $@"
 wait-for-url "$@"
+echo ""
+echo "SUCCESSFUL"
