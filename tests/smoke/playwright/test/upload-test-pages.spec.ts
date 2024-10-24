e34b952985ef379aadbd3e25b97747c7853a60cf
Tiffany Forkner <tforkner@fearless.tech>
2024-10-24 14:18:05 -0400

    resolved test issue

--- tests/smoke/playwright/test/upload-test-pages.spec.ts
+++ tests/smoke/playwright/test/upload-test-pages.spec.ts
@@ -50,7 +50,7 @@ test.describe("Upload Manifest Page", () => {
             const errorPageResponse = await errorPagePromise
     
             await expect(errorPageResponse.status()).toBe(404)
-            await expect(page.locator('body')).toContainText(`open v2/${dataStream}-${route}.json: `)
+            await expect(page.locator('body')).toContainText(`open v2/${dataStream}_${route}.json: `)
             await expect(page.locator('body')).toContainText('validation failure')
             await expect(page.locator('body')).toContainText('manifest validation config file not found')
         })
@@ -133,7 +133,7 @@ test.describe("Upload Status Page", () => {
         const fileHeaderContainer= page.locator('.file-header-container')
         await expect(fileHeaderContainer.getByRole('heading', { level: 1 }).nth(0)).toHaveText(expectedFileName)
         await expect(fileHeaderContainer.getByRole('heading', { level: 1 }).nth(1)).toHaveText("Upload Status: Complete")
-        await expect(fileHeaderContainer).toContainText(`ID: ${uploadId.split('+')[0]}`)
+        await expect(fileHeaderContainer).toContainText(`ID: ${uploadId}`)
     
         // const fileDeliveriesContainer = page.locator('.file-deliveries-container');
         // await expect(fileDeliveriesContainer.getByRole('heading', { level: 2 }).nth(0)).toHaveText('Delivery Status')
