ea9d29d4c8c0ce4bd23c4e1fd3d4315a5953ca64
David Gage <whytheplatypus@gmail.com>
2024-10-31 10:43:19 -0400

    unit test for getDeliveredFilename

---
+++ upload-server/internal/delivery/delivery_test.go
@@ -0,0 +1,44 @@
+package delivery
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestGetDeliveredFilename(t *testing.T) {
+	type testCase struct {
+		ctx          context.Context
+		tuid         string
+		pathTemplate string
+		manifest     map[string]string
+		err          error
+		result       string
+	}
+	ctx := context.Background()
+	testTime := time.Date(2020, time.April, 11, 15, 12, 30, 50, time.UTC)
+
+	testCases := []testCase{
+		{
+			ctx,
+			"bogus-id",
+			"routine-immunization-zip/{{.Year}}/{{.Month}}/{{.Day}}/{{.Filename}}",
+			map[string]string{
+				"dex_ingest_datetime": testTime.Format(time.RFC3339Nano),
+				"filename":            "test.txt",
+			},
+			nil,
+			"routine-immunization-zip/2020/04/11/test.txt",
+		},
+	}
+
+	for i, c := range testCases {
+		res, err := getDeliveredFilename(c.ctx, c.tuid, c.pathTemplate, c.manifest)
+		if res != c.result {
+			t.Errorf("missmatched results for test case %d: got %s expected %s", i, res, c.result)
+		}
+		if err != c.err {
+			t.Errorf("missmatched errors for test case %d: got %+v expected %+v", i, err, c.err)
+		}
+	}
+}
