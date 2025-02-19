a304aadc8c0db28681488078a33c3b0baf439efb
cfarmer-fearless <cfarmer@fearless.com>
2025-02-19 13:53:27 -0500

    remove unused get cookie function

--- upload-server/internal/ui/assets/tusclient.js
+++ upload-server/internal/ui/assets/tusclient.js
@@ -399,21 +399,6 @@ async function findResumableUpload() {
   return null;
 }
 
-function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
 (async () => {
   // initializes what is hidden/shown on the page base on
   // the uploadStatus and upload data in the local storage
