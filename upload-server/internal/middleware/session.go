84facdcf5047769117d7b124bfad7e9e0596f335
cfarmer-fearless <cfarmer@fearless.com>
2025-03-04 10:21:23 -0500

    log session values

--- upload-server/internal/middleware/session.go
+++ upload-server/internal/middleware/session.go
@@ -44,7 +44,7 @@ func GetUserSession(r *http.Request) (*UserSession, error) {
 }
 
 func (s *UserSession) Data() UserSessionData {
-	slog.Info("session values", "values", s.session.Values)
+	slog.Info("session", "token", s.session.Values["token"])
 	token, ok := s.session.Values["token"].(string)
 	if !ok {
 		token = ""
