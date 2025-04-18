14d9f99c6c52dba7a49f424db689b7b67b4dd0c0
Andrew Choi <ac45@cdc.gov>
2025-04-18 00:47:04 -0400

    use WithUploadIdLogger

--- upload-server/internal/logutil/setup.go
+++ upload-server/internal/logutil/setup.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/sloger"
 	"github.com/tus/tusd/v2/pkg/handler"
+	"github.com/tus/tusd/v2/pkg/hooks"
 )
 
 func SetupLogger(event *handler.HookEvent, uploadId string) *slog.Logger {
@@ -13,6 +14,13 @@ func SetupLogger(event *handler.HookEvent, uploadId string) *slog.Logger {
 	logger := sloger.GetLogger(event.Context)
 	logger.Info("Logger setup with upload ID")
 	return logger
+}
+
+func WithUploadIdLogger(event handler.HookEvent, resp hooks.HookResponse) (hooks.HookResponse, error) {
+	event.Context = sloger.SetUploadId(event.Context, event.Upload.ID)
+	logger := sloger.GetLogger(event.Context)
+	logger.Info("Logger setup with upload ID")
+	return resp, nil
 }
 
 func SetupLoggerWithContext(ctx context.Context, uploadId string) (context.Context, *slog.Logger) {
