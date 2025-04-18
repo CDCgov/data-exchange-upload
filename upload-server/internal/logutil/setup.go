f91604a383be953c7c94cff62c15d63e9181ecaf
Andrew Choi <ac45@cdc.gov>
2025-04-17 21:58:33 -0400

    use new context on SetUpLoadId

--- upload-server/internal/logutil/setup.go
+++ upload-server/internal/logutil/setup.go
@@ -1,23 +1,21 @@
 package logutil
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/cdcgov/data-exchange-upload/upload-server/pkg/sloger"
 	"github.com/tus/tusd/v2/pkg/handler"
-	"github.com/tus/tusd/v2/pkg/hooks"
 )
 
 func SetupLogger(event *handler.HookEvent, uploadId string) *slog.Logger {
+	// Create a new context for the event to avoid contamination
+	event.Context = context.WithValue(context.Background(), "parentContext", event.Context)
 	event.Context = sloger.SetUploadId(event.Context, uploadId)
 	logger := sloger.GetLogger(event.Context)
-	logger.Info("Logger setup with upload ID", uploadId)
-	return logger
-}
 
-func WithUploadIdLogger(event handler.HookEvent, resp hooks.HookResponse) (hooks.HookResponse, error) {
-	event.Context = sloger.SetUploadId(event.Context, event.Upload.ID)
-	slog.SetDefault(sloger.GetLogger(event.Context))
-	slog.Info("Logger setup with upload ID")
-	return resp, nil
+	// Log the setup
+	logger.Info("Logger setup with upload ID", "uploadId", uploadId)
+
+	return logger
 }
